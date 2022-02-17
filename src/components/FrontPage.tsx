e87fdb9bf93ba09001a5663cb5805d2b199fe2cc
mrjackmaster <jacektrg+github@gmail.com>
2022-02-18 00:30:12 +0100

    Basic Front Page for anonymous users

---
+++ src/components/FrontPage.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+import LogIn from './LogIn';
+import SignUp from './SignUp';
+
+const FrontPage = () => {
+	const [showLogIn, setShowLogIn] = useState(true);
+
+	return (
+		<div className='front-container'>
+			<div className='welcome'>Odinbook</div>
+			{showLogIn ? (
+				<LogIn setShowLogIn={setShowLogIn} />
+			) : (
+				<SignUp setShowLogIn={setShowLogIn} />
+			)}
+		</div>
+	);
+};
+
+export default FrontPage;
