81549c2a5bba6c0dc270d73754ce58b53c6c93f8
mrjackmaster <jacektrg+github@gmail.com>
2022-02-20 18:53:31 +0100

    Passing down props

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -2,12 +2,23 @@ import SideMenu from './SideMenu';
 import Timeline from './Timeline';
 import FriendList from './FriendList';
 
-const HomePage = () => {
+type Props = {
+	loggedUser: {
+		_id: string;
+		email: string;
+		first_name: string;
+		last_name: string;
+		friendList: string[];
+		friendRequiests: string[];
+	};
+};
+
+const HomePage: React.FC<Props> = ({ loggedUser }) => {
 	return (
 		<div className='home-container'>
 			<SideMenu />
-			<Timeline />
-			<FriendList />
+			<Timeline loggedUser={loggedUser} />
+			<FriendList loggedUser={loggedUser} />
 		</div>
 	);
 };
