e4accd0df4ccdabae8dacacb487886ece54bdbde
mrjackmaster <jacektrg+github@gmail.com>
2022-02-20 18:49:12 +0100

    Verifying user's saved token. Passing props.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+// @ts-nocheck
+
 import { useState, useEffect } from 'react';
 import { Navigate, useLocation } from 'react-router';
 import { Routes, Route, Outlet } from 'react-router-dom';
@@ -10,14 +12,21 @@ import HomePage from './components/HomePage';
 import MyProfile from './components/MyProfile';
 import UserProfile from './components/UserProfile';
 
+type StateObject = { _id: string; email: string } | null;
+
 const App = () => {
 	const location = useLocation();
-
 	const [isLoading, setIsLoading] = useState(true);
-	const [loggedUser, setLoggedUser] = useState(null);
+	const [loggedUser, setLoggedUser] = useState<StateObject>(null);
 
 	useEffect(() => {
-		(async () => {})();
+		(async () => {
+			const resUser = await axios.get('/api/verify-token', {
+				withCredentials: true,
+				headers: { 'Content-type': 'application/json' },
+			});
+			setLoggedUser(resUser.data);
+		})();
 	}, [location]);
 
 	return (
@@ -28,20 +37,20 @@ const App = () => {
 					<main className='main'>
 						{loggedUser ? (
 							<>
-								<Navbar />
+								<Navbar loggedUser={loggedUser} setLoggedUser={setLoggedUser} />
 								<Outlet />
 								<Footer />
 							</>
 						) : (
 							<>
-								<FrontPage />
+								<FrontPage setLoggedUser={setLoggedUser} />
 								<Footer />
 							</>
 						)}
 					</main>
 				}
 			>
-				<Route path='' element={<HomePage />} />
+				<Route path='' element={<HomePage loggedUser={loggedUser} />} />
 				<Route path='profile' element={<MyProfile />}></Route>
 				<Route path='user' element={<UserProfile />}></Route>
 			</Route>
