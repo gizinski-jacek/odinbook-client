198e0a6f2952d46adfee1563b1ae2db7f4f0adb0
mrjackmaster <jacektrg+github@gmail.com>
2022-02-23 20:23:56 +0100

    Cleanup after implemeting useContext hook for logged user's data. Moving post form to separate modal component.

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,25 +1,30 @@
-// @ts-nocheck
-
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import styles from '../styling/Timeline.module.scss';
+import { UserContext } from './hooks/UserContext';
+import PostFormModal from './utils/PostFormModal';
 import PostWrapper from './utils/PostWrapper';
 
-type Props = {
-	loggedUser: {
+type TimelinePosts = [
+	post: {
 		_id: string;
-		email: string;
-		first_name: string;
-		last_name: string;
-		friendList: string[];
-		friendRequiests: string[];
-	};
-};
+		author: {
+			_id: string;
+			first_name: string;
+			last_name: string;
+		};
+		text: string;
+		comments: string[];
+		likes: string[];
+		createdAt: string;
+		updatedAt: string;
+	}
+];
 
-const Timeline: React.FC<Props> = ({ loggedUser }) => {
-	const [showCreatePostModal, setShowCreatePostModal] = useState(false);
-	const [newPostFormData, setNewPostFormData] = useState({ text: '' });
-	const [timelinePosts, setTimelinePosts] = useState();
+const Timeline = () => {
+	const { user } = useContext(UserContext);
+	const [showPostFormModal, setShowPostFormModal] = useState(false);
+	const [timelinePosts, setTimelinePosts] = useState<TimelinePosts>();
 
 	useEffect(() => {
 		(async () => {
@@ -35,70 +40,37 @@ const Timeline: React.FC<Props> = ({ loggedUser }) => {
 		})();
 	}, []);
 
-	const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-		const { name, value } = e.target;
-		setNewPostFormData((prevState) => ({
-			...prevState,
-			[name]: value,
-		}));
-	};
-
-	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-		e.preventDefault();
-		try {
-			const resPosts = await axios.post('/api/posts', newPostFormData);
-			setTimelinePosts(resPosts.data);
-		} catch (error) {
-			console.error(error);
-		}
-	};
-
 	const toggleModal = (e: React.MouseEvent<HTMLDivElement>) => {
 		e.stopPropagation();
 		if (e.target === e.currentTarget) {
-			setShowCreatePostModal(false);
+			setShowPostFormModal(false);
 		}
 	};
 
 	const timelineDisplay = timelinePosts?.map((post) => {
-		return <PostWrapper key={post._id} post={post} />;
+		return (
+			<PostWrapper
+				key={post._id}
+				post={post}
+				setTimelinePosts={setTimelinePosts}
+			/>
+		);
 	});
 
 	return (
 		<div className={styles.posts}>
 			<div className='create-a-post'>
 				<img src='' alt='user-profile-pic' />
-				<div onClick={() => setShowCreatePostModal(true)}>
-					<h3>{`What's on your mind, ${loggedUser.first_name}?`}</h3>
-				</div>
+				<button onClick={() => setShowPostFormModal(true)}>
+					<h4>{`What's on your mind, ${user.first_name}?`}</h4>
+				</button>
 			</div>
-			{showCreatePostModal ? (
-				<div className='new-post-modal' onClick={(e) => toggleModal(e)}>
-					<div className='new-post-container'>
-						<div className='top'>
-							<h3>Create post</h3>
-							<span>
-								<img src='' alt='user-profile-pic' />
-								<h4>{`${loggedUser.first_name} ${loggedUser.last_name}`}</h4>
-							</span>
-						</div>
-						<form onSubmit={(e) => handleSubmit(e)}>
-							<textarea
-								id='text'
-								name='text'
-								minLength={8}
-								maxLength={512}
-								onChange={(e) => {
-									handleChange(e);
-								}}
-								value={newPostFormData.text}
-								required
-								placeholder={`What's on your mind, ${loggedUser.first_name}?`}
-							/>
-							<button type='submit'>Submit</button>
-						</form>
-					</div>
-				</div>
+			{showPostFormModal ? (
+				<PostFormModal
+					toggleModal={toggleModal}
+					setTimelinePosts={setTimelinePosts}
+					post={null}
+				/>
 			) : null}
 			{timelineDisplay ? timelineDisplay : null}
 		</div>
