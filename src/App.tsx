6b49265d313e222c0dd42a7a8627067035152605
mrjackmaster <jacektrg+github@gmail.com>
2022-02-23 19:58:10 +0100

    Implemeting useContext hook for logged user's data

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 // @ts-nocheck
 
 import { useState, useEffect } from 'react';
-import { Navigate, useLocation } from 'react-router';
+import { useLocation } from 'react-router';
 import { Routes, Route, Outlet } from 'react-router-dom';
+import { UserContext } from './components/hooks/UserContext';
 import axios from 'axios';
 import './App.scss';
 import Navbar from './components/Navbar';
@@ -13,12 +14,10 @@ import MyProfile from './components/MyProfile';
 import UserProfile from './components/UserProfile';
 import LoadingIcon from './components/utils/LoadingIcon';
 
-type StateObject = { _id: string; email: string } | null;
-
 const App = () => {
 	const location = useLocation();
 	const [isLoading, setIsLoading] = useState(true);
-	const [loggedUser, setLoggedUser] = useState<StateObject>(null);
+	const [user, setUser] = useState(null);
 
 	useEffect(() => {
 		(async () => {
@@ -26,7 +25,7 @@ const App = () => {
 				const resUser = await axios.get('/api/verify-token', {
 					withCredentials: true,
 				});
-				setLoggedUser(resUser.data);
+				setUser(resUser.data);
 				setIsLoading(false);
 			} catch (error) {
 				console.error(error);
@@ -35,33 +34,35 @@ const App = () => {
 	}, [location]);
 
 	return (
-		<Routes>
-			<Route
-				path='/'
-				element={
-					<main className='main'>
-						{isLoading ? (
-							<LoadingIcon />
-						) : loggedUser ? (
-							<>
-								<Navbar loggedUser={loggedUser} setLoggedUser={setLoggedUser} />
-								<Outlet />
-								<Footer />
-							</>
-						) : (
-							<>
-								<FrontPage setLoggedUser={setLoggedUser} />
-								<Footer />
-							</>
-						)}
-					</main>
-				}
-			>
-				<Route path='' element={<HomePage loggedUser={loggedUser} />} />
-				<Route path='profile' element={<MyProfile />}></Route>
-				<Route path='user' element={<UserProfile />}></Route>
-			</Route>
-		</Routes>
+		<UserContext.Provider value={{ user, setUser }}>
+			<Routes>
+				<Route
+					path='/'
+					element={
+						<main className='main'>
+							{isLoading ? (
+								<LoadingIcon />
+							) : user ? (
+								<>
+									<Navbar />
+									<Outlet />
+									<Footer />
+								</>
+							) : (
+								<>
+									<FrontPage />
+									<Footer />
+								</>
+							)}
+						</main>
+					}
+				>
+					<Route path='' element={<HomePage />} />
+					<Route path='profile' element={<MyProfile />}></Route>
+					<Route path='user' element={<UserProfile />}></Route>
+				</Route>
+			</Routes>
+		</UserContext.Provider>
 	);
 };
 
