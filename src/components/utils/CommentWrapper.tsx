d1a79cb6254052ff7d5cd8d970dd9ead5510f7b8
mrjackmaster <jacektrg+github@gmail.com>
2022-02-23 20:03:41 +0100

    Minor restructuring

--- src/components/utils/CommentWrapper.tsx
+++ src/components/utils/CommentWrapper.tsx
@@ -1,14 +1,29 @@
+import styles from '../../styling/Comment.module.scss';
+
 type Props = {
 	comment: {
 		_id: string;
 		text: string;
+		createdAt: string;
+		updatedAt: string;
 	};
 };
 
 const CommentWrapper: React.FC<Props> = ({ comment }) => {
 	return (
-		<div className='comment'>
-			<div>{comment.text}</div>
+		<div className={styles.comment}>
+			<img src='' alt='profile-pic' />
+			<div>
+				<div className='metadata'>
+					<h4>full name</h4>
+					<p>{comment.text}</p>
+				</div>
+				<span className='controls'>
+					<button>Like</button>
+					<button>Reply</button>
+					<p>{new Date(comment.createdAt).toLocaleString('en-bg')}</p>
+				</span>
+			</div>
 		</div>
 	);
 };
