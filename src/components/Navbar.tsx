5bd61de9f9994fc412be7b564ef6abc28f97e6b4
mrjackmaster <jacektrg+github@gmail.com>
2022-02-26 23:15:49 +0100

    Adding magnifying glass svg

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -91,18 +91,48 @@ const Navbar = () => {
 	return (
 		<nav className={styles.navbar}>
 			<div className={styles.left}>
-				<div className={styles.logo}>logo</div>
-				<input
-					className={styles.search}
-					type='text'
-					id='search_odinbook'
-					name='search_odinbook'
-					minLength={1}
-					maxLength={512}
-					value={searchOdinbookInput}
-					onChange={(e) => setSearchOdinbookInput(e.target.value)}
-					placeholder='Search Odinbook'
-				/>
+				<Link to='/'>
+					<div className='site-logo'>
+						<img src='placeholder_profile_pic.png' alt='user-profile-pic' />
+					</div>
+				</Link>
+				<label>
+					<span>
+						<svg viewBox='0 0 16 16' width='16' height='16'>
+							<g transform='translate(-448 -544)'>
+								<g>
+									<path
+										d='M10.743 2.257a6 6 0 1 1-8.485 8.486 6 6 0 0 1 8.485-8.486zm-1.06 1.06a4.5 4.5 0 1 0-6.365 6.364 4.5 4.5 0 0 0 6.364-6.363z'
+										transform='translate(448 544)'
+									></path>
+									<path
+										d='M10.39 8.75a2.94 2.94 0 0 0-.199.432c-.155.417-.23.849-.172 1.284.055.415.232.794.54 1.103a.75.75 0 0 0 1.112-1.004l-.051-.057a.39.39 0 0 1-.114-.24c-.021-.155.014-.356.09-.563.031-.081.06-.145.08-.182l.012-.022a.75.75 0 1 0-1.299-.752z'
+										transform='translate(448 544)'
+									></path>
+									<path
+										d='M9.557 11.659c.038-.018.09-.04.15-.064.207-.077.408-.112.562-.092.08.01.143.034.198.077l.041.036a.75.75 0 0 0 1.06-1.06 1.881 1.881 0 0 0-1.103-.54c-.435-.058-.867.018-1.284.175-.189.07-.336.143-.433.2a.75.75 0 0 0 .624 1.356l.066-.027.12-.061z'
+										transform='translate(448 544)'
+									></path>
+									<path
+										d='m13.463 15.142-.04-.044-3.574-4.192c-.599-.703.355-1.656 1.058-1.057l4.191 3.574.044.04c.058.059.122.137.182.24.249.425.249.96-.154 1.41l-.057.057c-.45.403-.986.403-1.411.154a1.182 1.182 0 0 1-.24-.182zm.617-.616.444-.444a.31.31 0 0 0-.063-.052c-.093-.055-.263-.055-.35.024l.208.232.207-.206.006.007-.22.257-.026-.024.033-.034.025.027-.257.22-.007-.007zm-.027-.415c-.078.088-.078.257-.023.35a.31.31 0 0 0 .051.063l.205-.204-.233-.209z'
+										transform='translate(448 544)'
+									></path>
+								</g>
+							</g>
+						</svg>
+					</span>
+					<input
+						className={styles.search}
+						type='text'
+						id='search_odinbook'
+						name='search_odinbook'
+						minLength={1}
+						maxLength={512}
+						value={searchOdinbookInput}
+						onChange={(e) => setSearchOdinbookInput(e.target.value)}
+						placeholder='Search Odinbook'
+					/>
+				</label>
 			</div>
 			<div className={styles.center}>
 				<ul className={styles.navigation}>
@@ -156,8 +186,8 @@ const Navbar = () => {
 						<span className={styles.icon}>
 							<svg
 								viewBox='0 0 20 20'
-								width='1em'
-								height='1em'
+								width='20'
+								height='20'
 								className='a8c37x1j ms05siws hwsy1cff b7h9ocf4 rs22bh7c fzdkajry jnigpg78 odw8uiq3'
 							>
 								<path d='M10 14a1 1 0 0 1-.755-.349L5.329 9.182a1.367 1.367 0 0 1-.205-1.46A1.184 1.184 0 0 1 6.2 7h7.6a1.18 1.18 0 0 1 1.074.721 1.357 1.357 0 0 1-.2 1.457l-3.918 4.473A1 1 0 0 1 10 14z'></path>
@@ -192,8 +222,8 @@ const Navbar = () => {
 						<span className={styles.icon}>
 							<svg
 								viewBox='0 0 44 44'
-								width='1em'
-								height='1em'
+								width='20'
+								height='20'
 								className='a8c37x1j ms05siws hwsy1cff b7h9ocf4 rs22bh7c fzdkajry jnigpg78 odw8uiq3'
 							>
 								<circle cx='7' cy='7' r='6'></circle>
@@ -209,15 +239,17 @@ const Navbar = () => {
 						</span>
 					</li>
 					<li>
-						<Link to='/me'>
-							{user.first_name}
+						<Link className={styles.me_link} to='/me'>
+							<h4>{user.first_name}</h4>
 							<div className='profile-picture'>
 								<img src='placeholder_profile_pic.png' alt='user-profile-pic' />
 							</div>
 						</Link>
 					</li>
 					<li>
-						<Link to='/friends'>Find Friends</Link>
+						<Link className={styles.find_friends_link} to='/friends'>
+							<h4>Find Friends</h4>
+						</Link>
 					</li>
 				</ul>
 			</div>
