a6b9718b4f78e121fc5a6f93a885265773e4635a
mrjackmaster <jacektrg+github@gmail.com>
2022-02-28 00:59:28 +0100

    Refactoring request and friend layouts

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -1,18 +1,19 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-import styles from './styling/Contacts.module.scss';
+import { Link } from 'react-router-dom';
+import styles from '../styles/Contacts.module.scss';
 
-type StateList = {
+type StateFriendList = {
 	_id: string;
 	first_name: string;
 	last_name: string;
 }[];
 
-type StateRequests = StateList;
+type StateFriendRequests = StateFriendList;
 
 const Contacts = () => {
-	const [friendList, setFriendList] = useState<StateList>([]);
-	const [friendRequests, setFriendRequests] = useState<StateRequests>([]);
+	const [friendList, setFriendList] = useState<StateFriendList>([]);
+	const [friendRequests, setFriendRequests] = useState<StateFriendRequests>([]);
 
 	useEffect(() => {
 		(async () => {
@@ -80,45 +81,59 @@ const Contacts = () => {
 		}
 	};
 
-	const friendListDisplay = friendList?.map((friend) => {
+	const friendRequestsDisplay = friendRequests?.map((request) => {
 		return (
-			<li key={friend._id} className={styles.friend}>
-				<div onClick={() => openChat(friend)}>
-					<div>profile picture</div>
-					<div>
-						<div>
-							{friend.first_name} {friend.last_name}
-						</div>
+			<li key={request._id} className={styles.request}>
+				<div className='profile-pic-style'>
+					<Link to={`/profile/${request._id}`}>
+						<img
+							src='icons/placeholder_profile_pic.png'
+							alt='user-profile-pic'
+						/>
+					</Link>
+				</div>
+				<div className={styles.contents}>
+					<h4>
+						{request.first_name} {request.last_name}
+					</h4>
+					<div className={styles.controls}>
+						<button
+							className='btn-confirm'
+							onClick={() => acceptRequest(request)}
+						>
+							Confirm
+						</button>
+						<button
+							className='btn-remove'
+							onClick={() => declineRequest(request)}
+						>
+							Remove
+						</button>
 					</div>
 				</div>
 			</li>
 		);
 	});
 
-	const friendRequestsDisplay = friendRequests?.map((request) => {
+	const friendListDisplay = friendList?.map((friend) => {
 		return (
-			<li key={request._id} className={styles.request}>
-				<div className={styles.info}>
-					<div>profile picture</div>
-					<div>
-						<div>
-							{request.first_name} {request.last_name}
-						</div>
-					</div>
+			<li
+				key={friend._id}
+				className={styles.friend}
+				onClick={() => openChat(friend)}
+			>
+				<div className='profile-pic-style'>
+					<Link to={`/profile/${friend._id}`}>
+						<img
+							src='icons/placeholder_profile_pic.png'
+							alt='user-profile-pic'
+						/>
+					</Link>
 				</div>
-				<div className={styles.controls}>
-					<button
-						className='button-accept'
-						onClick={() => acceptRequest(request)}
-					>
-						accept
-					</button>
-					<button
-						className='button-decline'
-						onClick={() => declineRequest(request)}
-					>
-						decline
-					</button>
+				<div>
+					<div>
+						{friend.first_name} {friend.last_name}
+					</div>
 				</div>
 			</li>
 		);
@@ -127,13 +142,22 @@ const Contacts = () => {
 	return (
 		<div className={styles.contacts}>
 			{friendRequests.length > 0 ? (
-				<>
-					<div>Requests</div>
-					<ul className={styles.friend_requests}>{friendRequestsDisplay}</ul>
-				</>
+				<div className={styles.friend_requests}>
+					<ul>
+						<div className={styles.top}>
+							<img src='icons/requests_icon.png' alt='requests-icon' />
+							<h5>Friend requests</h5>
+						</div>
+						{friendRequestsDisplay}
+					</ul>
+				</div>
 			) : null}
-			<div className={styles.contacts}>Contacts</div>
-			<ul className={styles.friend_list}>{friendListDisplay}</ul>
+			<div className={styles.friend_list}>
+				<div className={styles.top}>
+					<h3>Contacts</h3>
+				</div>
+				<ul>{friendListDisplay}</ul>
+			</div>
 		</div>
 	);
 };
