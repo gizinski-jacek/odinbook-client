37db6b330a87514040902fe24448260a945b3dc4
mrjackmaster <jacektrg+github@gmail.com>
2022-03-03 18:09:34 +0100

    Refactoring modal open/close. Moving edit comment form functionality to separate component. Moving delete modal to separate component.

--- src/components/utils/CommentWrapper.tsx
+++ src/components/utils/CommentWrapper.tsx
@@ -5,6 +5,8 @@ import { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 import styles from '../../styles/Comment.module.scss';
 import { UserContext } from '../hooks/UserContext';
+import DeleteModal from './DeleteModal';
+import EditCommentForm from './EditCommentForm';
 import dateFormatter from './_dateFormatter';
 
 type Props = {
@@ -25,10 +27,9 @@ type Props = {
 
 const CommentWrapper: React.FC<Props> = ({ comment, setCommentsData }) => {
 	const { user } = useContext(UserContext);
-	const [commentFormData, setCommentFormData] = useState({ text: '' });
 	const [editingComment, setEditingComment] = useState(false);
 	const [showOptions, setShowOptions] = useState(false);
-	const [showConfirmDeleteModal, setShowConfirmDeleteModal] = useState(false);
+	const [showModal, setShowModal] = useState(false);
 
 	const handleLike = async (commentId: string) => {
 		try {
@@ -66,14 +67,6 @@ const CommentWrapper: React.FC<Props> = ({ comment, setCommentsData }) => {
 		}
 	};
 
-	const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-		const { name, value } = e.target;
-		setCommentFormData((prevState) => ({
-			...prevState,
-			[name]: value,
-		}));
-	};
-
 	const handleDelete = async (commentPostRef: string, commentId: string) => {
 		try {
 			const resCommentList = await axios.delete(
@@ -96,16 +89,6 @@ const CommentWrapper: React.FC<Props> = ({ comment, setCommentsData }) => {
 		}
 	};
 
-	const toggleDeleteModal = (e: React.MouseEvent<HTMLDivElement>) => {
-		e.stopPropagation();
-		if (e.target !== e.currentTarget) {
-			setShowConfirmDeleteModal(true);
-			document.addEventListener('click', closeOptions);
-		} else {
-			setShowConfirmDeleteModal(false);
-		}
-	};
-
 	const closeOptions = (e: any) => {
 		e.stopPropagation();
 		if (
@@ -124,9 +107,25 @@ const CommentWrapper: React.FC<Props> = ({ comment, setCommentsData }) => {
 		}
 	};
 
+	const openDeleteModal = (e: React.MouseEvent<HTMLDivElement>) => {
+		e.stopPropagation();
+		setShowModal(true);
+	};
+
+	const closeDeleteModal = (e: React.MouseEvent<HTMLDivElement>) => {
+		e.stopPropagation();
+		setShowModal(false);
+	};
+
 	return (
 		<>
-			{!editingComment ? (
+			{editingComment ? (
+				<EditCommentForm
+					handleUpdate={handleUpdate}
+					setEditingComment={setEditingComment}
+					comment={comment}
+				/>
+			) : (
 				<div className={styles.comment}>
 					<div className='profile-pic-style'>
 						<Link to={`/profile/${comment.author._id}`}>
@@ -137,11 +136,41 @@ const CommentWrapper: React.FC<Props> = ({ comment, setCommentsData }) => {
 						</Link>
 					</div>
 					<div className={styles.body}>
-						<div className={styles.metadata}>
-							<Link to={`/profile/${comment.author._id}`}>
-								{comment.author.first_name} {comment.author.last_name}
-							</Link>
-							<p>{comment.text}</p>
+						<div className={styles.upper}>
+							<div className={styles.contents}>
+								<Link to={`/profile/${comment.author._id}`}>
+									{comment.author.first_name} {comment.author.last_name}
+								</Link>
+								<p>{comment.text}</p>
+							</div>
+							{user._id === comment.author._id ? (
+								<span
+									className={styles.options_toggle}
+									onClick={(e) => toggleOptions(e)}
+								>
+									<svg viewBox='0 0 20 20' width='20' height='20'>
+										<g transform='translate(-446 -350)'>
+											<path d='M458 360a2 2 0 1 1-4 0 2 2 0 0 1 4 0m6 0a2 2 0 1 1-4 0 2 2 0 0 1 4 0m-12 0a2 2 0 1 1-4 0 2 2 0 0 1 4 0'></path>
+										</g>
+									</svg>
+									{showOptions ? (
+										<div className={styles.options_menu}>
+											<div
+												className={styles.edit_btn}
+												onClick={(e) => toggleCommentForm(e)}
+											>
+												Edit comment
+											</div>
+											<div
+												className={styles.delete_btn}
+												onClick={(e) => openDeleteModal(e)}
+											>
+												Delete comment
+											</div>
+										</div>
+									) : null}
+								</span>
+							) : null}
 						</div>
 						<span className={styles.controls}>
 							<div
@@ -161,101 +190,14 @@ const CommentWrapper: React.FC<Props> = ({ comment, setCommentsData }) => {
 							</Link>
 						</span>
 					</div>
-					{user._id === comment.author._id ? (
-						<span
-							className={styles.options_toggle}
-							onClick={(e) => toggleOptions(e)}
-						>
-							<svg viewBox='0 0 20 20' width='20' height='20'>
-								<g transform='translate(-446 -350)'>
-									<path d='M458 360a2 2 0 1 1-4 0 2 2 0 0 1 4 0m6 0a2 2 0 1 1-4 0 2 2 0 0 1 4 0m-12 0a2 2 0 1 1-4 0 2 2 0 0 1 4 0'></path>
-								</g>
-							</svg>
-							{showOptions ? (
-								<div className={styles.options_menu}>
-									<div
-										className={styles.edit_btn}
-										onClick={(e) => toggleCommentForm(e)}
-									>
-										Edit comment
-									</div>
-									<div
-										className={styles.delete_btn}
-										onClick={() => setShowConfirmDeleteModal(true)}
-									>
-										Delete comment
-									</div>
-								</div>
-							) : null}
-						</span>
-					) : null}
-					{showConfirmDeleteModal ? (
-						<div
-							className={styles.confirm_delete_modal}
-							onClick={(e) => toggleDeleteModal(e)}
-						>
-							<div className={styles.confirm_delete}>
-								<h3>Delete this comment?</h3>
-								<span>Are you sure you want to delete this comment?</span>
-								<div className={styles.delete_controls}>
-									<button
-										type='button'
-										className='btn-default btn-cancel'
-										onClick={(e) => toggleDeleteModal(e)}
-									>
-										Cancel
-									</button>
-									<button
-										type='button'
-										className='btn-default btn-confirm'
-										onClick={() => handleDelete(comment.post_ref, comment._id)}
-									>
-										Delete
-									</button>
-								</div>
-							</div>
-						</div>
-					) : null}
-				</div>
-			) : (
-				<div className={styles.edit_comment}>
-					<div className='profile-pic-style'>
-						<Link to={`/profile/${comment.author._id}`}>
-							<img
-								src='icons/placeholder_profile_pic.png'
-								alt='user-profile-pic'
-							/>
-						</Link>
-					</div>
-					<form
-						onSubmit={(e) => handleUpdate(e, comment.post_ref, comment._id)}
-					>
-						<textarea
-							id='text'
-							name='text'
-							minLength={1}
-							maxLength={512}
-							onChange={(e) => handleChange(e)}
-							value={commentFormData.text ? commentFormData.text : comment.text}
-							required
-							placeholder='Write a comment...'
+
+					{showModal ? (
+						<DeleteModal
+							closeModal={closeDeleteModal}
+							handleDelete={handleDelete}
+							text={'comment'}
 						/>
-						<div className={styles.edit_controls}>
-							<button
-								type='button'
-								className='btn-default btn-cancel'
-								onClick={() => {
-									setCommentFormData({ text: '' });
-									setEditingComment(false);
-								}}
-							>
-								Cancel
-							</button>
-							<button type='submit' className='btn-default btn-form-submit'>
-								Update
-							</button>
-						</div>
-					</form>
+					) : null}
 				</div>
 			)}
 		</>
