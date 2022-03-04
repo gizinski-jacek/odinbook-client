daeafc455fe1e91ae3d1268180b4cc80a03927ca
mrjackmaster <jacektrg+github@gmail.com>
2022-03-04 21:19:38 +0100

    Minor changes to classes

--- src/components/FrontPage.tsx
+++ src/components/FrontPage.tsx
@@ -6,13 +6,18 @@ const FrontPage = () => {
 	const [showLogIn, setShowLogIn] = useState(true);
 
 	return (
-		<div className='front-container'>
-			<div className='welcome'>Odinbook</div>
-			{showLogIn ? (
-				<LogIn setShowLogIn={setShowLogIn} />
-			) : (
-				<SignUp setShowLogIn={setShowLogIn} />
-			)}
+		<div className='welcome-page-container'>
+			<div className='left'>
+				<h1>Odinbook</h1>
+				<h2>Helps you connect and share with the people in your life</h2>
+			</div>
+			<div className='right'>
+				{showLogIn ? (
+					<LogIn setShowLogIn={setShowLogIn} />
+				) : (
+					<SignUp setShowLogIn={setShowLogIn} />
+				)}
+			</div>
 		</div>
 	);
 };
