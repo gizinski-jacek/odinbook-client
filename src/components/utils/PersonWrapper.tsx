bddf77be153bf61502ab36f88186f9490fd9e74e
mrjackmaster <jacektrg+github@gmail.com>
2022-03-05 22:25:54 +0100

    Renaming component

---
+++ src/components/utils/PersonWrapper.tsx
@@ -0,0 +1,36 @@
+import { Link } from 'react-router-dom';
+import styles from '../../styles/Person.module.scss';
+import type { User } from '../../myTypesTS';
+
+type Props = {
+	handleSendRequest: Function;
+	person: User;
+};
+
+const PersonWrapper: React.FC<Props> = ({ person, handleSendRequest }) => {
+	return (
+		<li className={styles.not_friend}>
+			<Link to={`/profile/${person._id}`}>
+				<div className={styles.pic_link}>
+					<img src='placeholder_profile_pic.png' alt='User profile pic' />
+				</div>
+			</Link>
+			<Link to={`/profile/${person._id}`}>
+				<h4>{person.full_name}</h4>
+			</Link>
+			<div className={styles.controls}>
+				<button
+					type='button'
+					className='btn-default btn-active'
+					onClick={() => {
+						handleSendRequest(person._id);
+					}}
+				>
+					Add Friend
+				</button>
+			</div>
+		</li>
+	);
+};
+
+export default PersonWrapper;
