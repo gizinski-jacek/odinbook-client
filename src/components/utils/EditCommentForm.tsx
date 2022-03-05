261c95cd49e66a9dafa5357fc2b5460a9fd90c8a
mrjackmaster <jacektrg+github@gmail.com>
2022-03-05 23:33:28 +0100

    Importing types. Update, renaming and cleanup.

--- src/components/utils/EditCommentForm.tsx
+++ src/components/utils/EditCommentForm.tsx
@@ -1,33 +1,24 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import styles from '../../styles/EditCommentForm.module.scss';
+import type { Comment } from '../../myTypes';
 
 type Props = {
 	handleUpdate: Function;
-	setEditingComment: Function;
-	comment: {
-		_id: string;
-		author: {
-			_id: string;
-			first_name: string;
-			last_name: string;
-		};
-		post_ref: string;
-		text: string;
-		createdAt: string;
-		updatedAt: string;
-	};
+	setEditComment: Function;
+	comment: Comment;
 };
+
 const EditCommentForm: React.FC<Props> = ({
 	handleUpdate,
-	setEditingComment,
+	setEditComment,
 	comment,
 }) => {
-	const [commentFormData, setCommentFormData] = useState(comment);
+	const [formData, setFormData] = useState(comment);
 
 	const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
 		const { name, value } = e.target;
-		setCommentFormData((prevState) => ({
+		setFormData((prevState) => ({
 			...prevState,
 			[name]: value,
 		}));
@@ -37,12 +28,12 @@ const EditCommentForm: React.FC<Props> = ({
 		<div className={styles.edit_comment}>
 			<div className='profile-pic-style'>
 				<Link to={`/profile/${comment.author._id}`}>
-					<img src='placeholder_profile_pic.png' alt='User profile picture' />
+					<img src='placeholder_profile_pic.png' alt='User profile pic' />
 				</Link>
 			</div>
 			<form
 				onSubmit={(e) =>
-					handleUpdate(e, comment.post_ref, comment._id, commentFormData)
+					handleUpdate(e, comment.post_ref, comment._id, formData)
 				}
 			>
 				<textarea
@@ -51,7 +42,7 @@ const EditCommentForm: React.FC<Props> = ({
 					minLength={1}
 					maxLength={512}
 					onChange={(e) => handleChange(e)}
-					value={commentFormData.text}
+					value={formData.text}
 					required
 					placeholder='Write a comment...'
 				/>
@@ -59,7 +50,7 @@ const EditCommentForm: React.FC<Props> = ({
 					<button
 						type='button'
 						className='btn-default btn-cancel'
-						onClick={() => setEditingComment(false)}
+						onClick={() => setEditComment(false)}
 					>
 						Cancel
 					</button>
