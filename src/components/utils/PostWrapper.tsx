4ab8b2d593ab801ebe0a0d4b4f40cf23ac168b14
mrjackmaster <jacektrg+github@gmail.com>
2022-03-05 23:39:25 +0100

    Cleanup

--- src/components/utils/PostWrapper.tsx
+++ src/components/utils/PostWrapper.tsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 import styles from '../../styles/Post.module.scss';
 import CommentWrapper from './CommentWrapper';
