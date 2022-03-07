1236ad059265c857a589775a256dc1f24c55e4b9
mrjackmaster <jacektrg+github@gmail.com>
2022-03-08 00:19:03 +0100

    Adding liked styling and icon. Useffect fetching comment data in case its not present. Cleanup after moving axios functionality to separate file. Moving delete function inside modal

--- src/components/utils/CommentWrapper.tsx
+++ src/components/utils/CommentWrapper.tsx
@@ -1,33 +1,49 @@
-import axios from 'axios';
-import { useContext, useState } from 'react';
-import { Link } from 'react-router-dom';
-import styles from '../../styles/Comment.module.scss';
+import { useContext, useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
 import { UserContext } from '../hooks/UserContext';
-import DeleteModal from './DeleteModal';
+import type { CommentFull } from '../../myTypes';
+import { axiosGet, axiosPut } from './axiosFunctions';
 import EditCommentForm from './EditCommentForm';
+import DeleteModal from './DeleteModal';
 import timeSinceDate from './_timeSinceDate';
-import type { Comment } from '../../myTypes';
+import styles from '../../styles/Comment.module.scss';
 
 type Props = {
-	setCommentsData: Function;
-	comment: Comment;
+	comment: CommentFull;
 };
 
-const CommentWrapper: React.FC<Props> = ({ comment, setCommentsData }) => {
+const CommentWrapper: React.FC<Props> = ({ comment }) => {
 	const { user } = useContext(UserContext);
 
+	const params = useParams();
+
+	const [commentData, setCommentData] = useState<CommentFull>(comment);
 	const [editComment, setEditComment] = useState(false);
 	const [showOptions, setShowOptions] = useState(false);
 	const [showModal, setShowModal] = useState(false);
 
+	useEffect(() => {
+		(async () => {
+			if (!comment) {
+				try {
+					setCommentData(
+						await axiosGet(`/api/posts/${params.commentId}/comments`)
+					);
+				} catch (error: any) {
+					console.error(error);
+				}
+			}
+		})();
+	}, [comment, params, setCommentData]);
+
 	const handleLike = async (commentPostRef: string, commentId: string) => {
 		try {
-			const resCommentList = await axios.put(
-				`/api/posts/${commentPostRef}/comments/${commentId}/like`,
-				{ commentPostRef, commentId },
-				{ withCredentials: true }
+			setCommentData(
+				await axiosPut(
+					`/api/posts/${commentPostRef}/comments/${commentId}/like`,
+					{ commentPostRef, commentId }
+				)
 			);
-			setCommentsData(resCommentList);
 		} catch (error: any) {
 			console.error(error);
 		}
@@ -42,28 +58,17 @@ const CommentWrapper: React.FC<Props> = ({ comment, setCommentsData }) => {
 
 	const handleUpdate = async (
 		e: React.FormEvent<HTMLFormElement>,
-		formData: Comment
+		formData: CommentFull
 	) => {
 		e.preventDefault();
 		try {
-			const resCommentList = await axios.put(
-				`/api/posts/${formData.post_ref}/comments/${formData._id}`,
-				formData,
-				{ withCredentials: true }
+			setCommentData(
+				await axiosPut(
+					`/api/posts/${formData.post_ref}/comments/${formData._id}`,
+					formData
+				)
 			);
-			setCommentsData(resCommentList.data);
-		} catch (error: any) {
-			console.error(error);
-		}
-	};
-
-	const handleDelete = async (commentPostRef: string, commentId: string) => {
-		try {
-			const resCommentList = await axios.delete(
-				`/api/posts/${commentPostRef}/comments/${commentId}`,
-				{ withCredentials: true }
-			);
-			setCommentsData(resCommentList.data);
+			setEditComment(false);
 		} catch (error: any) {
 			console.error(error);
 		}
@@ -90,9 +95,7 @@ const CommentWrapper: React.FC<Props> = ({ comment, setCommentsData }) => {
 
 	const toggleCommentForm = (e: React.MouseEvent<HTMLDivElement>) => {
 		e.stopPropagation();
-		if (e.target === e.currentTarget) {
-			setEditComment(true);
-		}
+		setEditComment(true);
 	};
 
 	const openDeleteModal = (e: React.MouseEvent<HTMLDivElement>) => {
@@ -105,96 +108,99 @@ const CommentWrapper: React.FC<Props> = ({ comment, setCommentsData }) => {
 		setShowModal(false);
 	};
 
-	return (
-		<>
-			{editComment ? (
-				<EditCommentForm
-					handleUpdate={handleUpdate}
-					setEditComment={setEditComment}
-					comment={comment}
-				/>
-			) : (
-				<div className={styles.comment}>
-					<div className='profile-pic-style'>
-						<Link to={`/profile/${comment.author._id}`}>
-							<img src='/placeholder_profile_pic.png' alt='User profile pic' />
+	return editComment ? (
+		<EditCommentForm
+			handleUpdate={handleUpdate}
+			setEditComment={setEditComment}
+			comment={comment}
+		/>
+	) : (
+		<div className={styles.comment}>
+			<div className='profile-pic-style'>
+				<Link to={`/profile/${commentData.author._id}`}>
+					<img src='/placeholder_profile_pic.png' alt='User profile pic' />
+				</Link>
+			</div>
+			<div className={styles.body}>
+				<div className={styles.upper}>
+					<div className={styles.contents}>
+						<Link to={`/profile/${commentData.author._id}`}>
+							{commentData.author.full_name}
 						</Link>
-					</div>
-					<div className={styles.body}>
-						<div className={styles.upper}>
-							<div className={styles.contents}>
-								<Link to={`/profile/${comment.author._id}`}>
-									{comment.author.full_name}
-								</Link>
-								<p>{comment.text}</p>
+						<p>{commentData.text}</p>
+						{commentData.likes.includes(user._id) ? (
+							<div className={styles.liked}>
+								<span></span>
 							</div>
-							<div className={styles.right}>
-								{user._id === comment.author._id ? (
-									<>
-										<span
-											className={styles.options_toggle}
-											onClick={(e) => toggleOptions(e)}
+						) : null}
+					</div>
+					<div className={styles.right}>
+						{user._id === commentData.author._id ? (
+							<>
+								<span
+									className={styles.options_toggle}
+									onClick={(e) => toggleOptions(e)}
+								>
+									<svg viewBox='0 0 20 20' width='20' height='20'>
+										<g transform='translate(-446 -350)'>
+											<path d='M458 360a2 2 0 1 1-4 0 2 2 0 0 1 4 0m6 0a2 2 0 1 1-4 0 2 2 0 0 1 4 0m-12 0a2 2 0 1 1-4 0 2 2 0 0 1 4 0'></path>
+										</g>
+									</svg>
+								</span>
+								{showOptions ? (
+									<div className={styles.options_menu}>
+										<div
+											className={styles.edit_btn}
+											onClick={(e) => {
+												toggleCommentForm(e);
+												closeOptions(e);
+											}}
 										>
-											<svg viewBox='0 0 20 20' width='20' height='20'>
-												<g transform='translate(-446 -350)'>
-													<path d='M458 360a2 2 0 1 1-4 0 2 2 0 0 1 4 0m6 0a2 2 0 1 1-4 0 2 2 0 0 1 4 0m-12 0a2 2 0 1 1-4 0 2 2 0 0 1 4 0'></path>
-												</g>
-											</svg>
-										</span>
-										{showOptions ? (
-											<div className={styles.options_menu}>
-												<div
-													className={styles.edit_btn}
-													onClick={(e) => {
-														toggleCommentForm(e);
-														closeOptions(e);
-													}}
-												>
-													Edit comment
-												</div>
-												<div
-													className={styles.delete_btn}
-													onClick={(e) => {
-														openDeleteModal(e);
-														closeOptions(e);
-													}}
-												>
-													Delete comment
-												</div>
-											</div>
-										) : null}
-									</>
+											Edit comment
+										</div>
+										<div
+											className={styles.delete_btn}
+											onClick={(e) => {
+												openDeleteModal(e);
+												closeOptions(e);
+											}}
+										>
+											Delete comment
+										</div>
+									</div>
 								) : null}
-							</div>
-						</div>
-						<span className={styles.controls}>
-							<div
-								className={styles.like_btn}
-								onClick={() => handleLike(comment.post_ref, comment._id)}
-							>
-								Like
-							</div>
-							<div
-								className={styles.reply_btn}
-								onClick={() => handleReply(comment._id)}
-							>
-								Reply
-							</div>
-							<Link to={`/posts/:postid`}>
-								{timeSinceDate(comment.createdAt)}
-							</Link>
-						</span>
+							</>
+						) : null}
 					</div>
-					{showModal ? (
-						<DeleteModal
-							closeModal={closeDeleteModal}
-							handleDelete={handleDelete}
-							item={'comment'}
-						/>
-					) : null}
 				</div>
-			)}
-		</>
+				<span className={styles.controls}>
+					<div
+						className={`${styles.like_btn} ${
+							commentData.likes.includes(user._id) ? styles.liked : ''
+						}`}
+						onClick={() => handleLike(commentData.post_ref, commentData._id)}
+					>
+						Like
+					</div>
+					<div
+						className={styles.reply_btn}
+						onClick={() => handleReply(commentData._id)}
+					>
+						Reply
+					</div>
+					<div className={styles.time}>
+						{timeSinceDate(commentData.createdAt)}
+					</div>
+				</span>
+			</div>
+			{showModal ? (
+				<DeleteModal
+					closeModal={closeDeleteModal}
+					setData={setCommentData}
+					comment={commentData}
+				/>
+			) : null}
+		</div>
 	);
 };
 
