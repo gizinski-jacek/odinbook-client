1a358c121b94a73fbb534249f91e5cb967306b23
mrjackmaster <jacektrg+github@gmail.com>
2022-03-08 00:16:36 +0100

    No changes

--- src/components/utils/RequestWrapper.tsx
+++ src/components/utils/RequestWrapper.tsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
-import styles from '../../styles/Request.module.scss';
 import type { User } from '../../myTypes';
+import styles from '../../styles/Request.module.scss';
 
 type Props = {
 	acceptRequest: Function;
