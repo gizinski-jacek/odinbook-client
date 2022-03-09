7902b6146d440f4825807c43bf883e7efc4b7e17
mrjackmaster <jacektrg+github@gmail.com>
2022-03-09 21:00:27 +0100

    Added search posts functionality. Removed search from menu. Added Navlinks for styling. Cleanup/renaming.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,9 @@
-import axios from 'axios';
-import { useContext, useState } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
 import { UserContext } from './hooks/UserContext';
+import type { PostFull, User } from '../myTypes';
+import { axiosGet } from './utils/axiosFunctions';
+import RequestWrapper from './utils/RequestWrapper';
 import styles from '../styles/Navbar.module.scss';
 
 const Navbar = () => {
@@ -9,18 +11,34 @@ const Navbar = () => {
 
 	const navigate = useNavigate();
 
+	const allRef = useRef(null);
+	const unreadRef = useRef(null);
+
+	const [showNotificationsRequests, setShowNotificationsRequests] =
+		useState<User[]>();
 	const [showMenuContainer, setShowMenuContainer] = useState(false);
 	const [showMainMenu, setShowMainMenu] = useState(false);
 	const [showMessengerMenu, setShowMessengerMenu] = useState(false);
 	const [showNotificationsMenu, setShowNotificationsMenu] = useState(false);
 	const [showAccountMenu, setShowAccountMenu] = useState(false);
-	const [searchOdinbookInput, setSearchOdinbookInput] = useState('');
+	const [searchInput, setSearchInput] = useState('');
 	const [searchMessengerInput, setSearchMessengerInput] = useState('');
+	const [searchData, setSearchData] = useState<PostFull[] | null>();
 	const [showAllNotifications, setShowAllNotifications] = useState(true);
+
+	// useEffect(() => {
+	// 	(async () => {
+	// 		try {
+	// 			setShowNotificationsRequests(await axiosGet('/api/users/requests'));
+	// 		} catch (error: any) {
+	// 			console.error(error);
+	// 		}
+	// 	})();
+	// }, []);
 
 	const handleLogOut = async () => {
 		try {
-			await axios.get('/api/log-out');
+			await axiosGet('/api/log-out');
 			setUser(null);
 			navigate('/');
 		} catch (error: any) {
@@ -28,11 +46,11 @@ const Navbar = () => {
 		}
 	};
 
-	const toggleMenu = (e: any) => {
+	const toggleMenu = (e: React.MouseEvent<HTMLLIElement>) => {
 		e.stopPropagation();
 		if (!showMenuContainer) {
 			setShowMenuContainer(true);
-			window.addEventListener('click', closeMenuContainer);
+			document.addEventListener('click', closeMenuContainer);
 		}
 		switch (e.currentTarget.className) {
 			case 'account':
@@ -89,21 +107,69 @@ const Navbar = () => {
 			setShowNotificationsMenu(false);
 			setShowAccountMenu(false);
 			setShowMenuContainer(false);
-			window.removeEventListener('click', closeMenuContainer);
+			document.removeEventListener('click', closeMenuContainer);
 		}
 	};
 
 	const changeNotificationList = (e: any) => {
 		e.stopPropagation();
-		if (e.target.className.includes('all')) {
+		if (e.target === allRef.current) {
 			// fetch all notifications
 			setShowAllNotifications(true);
 		}
-		if (e.target.className.includes('unread')) {
+		if (e.target === unreadRef.current) {
 			// fetch unread notifications
 			setShowAllNotifications(false);
 		}
 	};
+
+	const notificationsRequestsDisplay = showNotificationsRequests?.map(
+		(request) => {
+			return;
+			// return (
+			// 	<RequestWrapper
+			// 		acceptRequest={request}
+			// 		declineRequest={request}
+			// 		key={request._id}
+			// 	/>
+			// );
+		}
+	);
+
+	const handleSearch = async (
+		e: React.FormEvent<HTMLFormElement>,
+		query: string
+	) => {
+		e.preventDefault();
+		try {
+			setSearchData(await axiosGet(`/api/posts/search?q=${query}`));
+		} catch (error: any) {
+			console.error(error);
+		}
+	};
+
+	const clearSearch = (e: React.MouseEvent<HTMLElement>) => {
+		e.stopPropagation();
+		setSearchInput('');
+		setSearchData(null);
+	};
+
+	const searchDisplay = searchData?.map((item) => {
+		return (
+			<li key={item._id} onClick={(e) => clearSearch(e)}>
+				<div className={styles.search_item}>
+					<div className='profile-pic-style'>
+						<Link to={`/profile/${item.author._id}`}>
+							<img src='/placeholder_profile_pic.png' alt='User profile pic' />
+						</Link>
+					</div>
+					<Link to={`/posts/${item._id}`}>
+						<span className={styles.text}>{item.text}</span>
+					</Link>
+				</div>
+			</li>
+		);
+	});
 
 	return (
 		<nav className={styles.navbar}>
@@ -136,7 +202,10 @@ const Navbar = () => {
 						</svg>
 					</div>
 				</Link>
-				<div className={styles.search_odinbook}>
+				<form
+					className={styles.search_posts}
+					onSubmit={(e) => handleSearch(e, searchInput)}
+				>
 					<label>
 						<span>
 							<svg viewBox='0 0 16 16' width='16' height='16'>
@@ -164,16 +233,30 @@ const Navbar = () => {
 						</span>
 						<input
 							type='text'
-							id='search_odinbook'
-							name='search_odinbook'
+							id='search_posts'
+							name='search_posts'
 							minLength={1}
 							maxLength={512}
-							value={searchOdinbookInput}
-							onChange={(e) => setSearchOdinbookInput(e.target.value)}
-							placeholder='Search Odinbook'
+							value={searchInput}
+							onChange={(e) => setSearchInput(e.target.value)}
+							placeholder='Search Posts'
 						/>
+						<div
+							style={{
+								visibility: searchInput || searchData ? 'visible' : 'hidden',
+							}}
+							className={styles.clear_btn}
+							onClick={(e) => clearSearch(e)}
+						>
+							<span></span>
+						</div>
 					</label>
-				</div>
+					{searchDisplay && searchDisplay.length > 0 ? (
+						<div className={styles.search_posts_container}>
+							<ul>{searchDisplay}</ul>
+						</div>
+					) : null}
+				</form>
 			</div>
 			<div className={styles.center}>
 				<ul className={styles.navigation}>
@@ -307,7 +390,12 @@ const Navbar = () => {
 						</span>
 					</li>
 					<li>
-						<Link to='/me' className={styles.me_link}>
+						<NavLink
+							to={`/profile/${user._id}`}
+							className={({ isActive }) =>
+								isActive ? `${styles.me_link} btn-active` : styles.me_link
+							}
+						>
 							<h4>{user.first_name}</h4>
 							<div className='profile-pic-style'>
 								<img
@@ -315,12 +403,19 @@ const Navbar = () => {
 									alt='User profile pic'
 								/>
 							</div>
-						</Link>
+						</NavLink>
 					</li>
 					<li>
-						<Link to='/friends' className={styles.find_friends_link}>
+						<NavLink
+							to='/friends'
+							className={({ isActive }) =>
+								isActive
+									? `${styles.friends_link} btn-active`
+									: styles.friends_link
+							}
+						>
 							<h5>Find Friends</h5>
-						</Link>
+						</NavLink>
 					</li>
 				</ul>
 			</div>
@@ -401,6 +496,7 @@ const Navbar = () => {
 							</div>
 							<div className={styles.controls}>
 								<div
+									ref={allRef}
 									className={`btn-default ${
 										showAllNotifications ? 'btn-active' : ''
 									}`}
@@ -409,6 +505,7 @@ const Navbar = () => {
 									All
 								</div>
 								<div
+									ref={unreadRef}
 									className={`btn-default ${
 										showAllNotifications ? '' : 'btn-active'
 									}`}
@@ -419,9 +516,9 @@ const Navbar = () => {
 							</div>
 							<div className={styles.notification_list}>
 								<ul>
-									<li>Notification 1</li>
-									<li>Notification 2</li>
-									<li>Notification 3</li>
+									{notificationsRequestsDisplay
+										? notificationsRequestsDisplay
+										: null}
 								</ul>
 							</div>
 						</div>
@@ -492,44 +589,6 @@ const Navbar = () => {
 							<h2>Menu</h2>
 							<div className={styles.both_sides}>
 								<div className={styles.left_side}>
-									<div className={styles.menu_search_odinbook}>
-										<label>
-											<span>
-												<svg viewBox='0 0 16 16' width='16' height='16'>
-													<g transform='translate(-448 -544)'>
-														<g>
-															<path
-																d='M10.743 2.257a6 6 0 1 1-8.485 8.486 6 6 0 0 1 8.485-8.486zm-1.06 1.06a4.5 4.5 0 1 0-6.365 6.364 4.5 4.5 0 0 0 6.364-6.363z'
-																transform='translate(448 544)'
-															></path>
-															<path
-																d='M10.39 8.75a2.94 2.94 0 0 0-.199.432c-.155.417-.23.849-.172 1.284.055.415.232.794.54 1.103a.75.75 0 0 0 1.112-1.004l-.051-.057a.39.39 0 0 1-.114-.24c-.021-.155.014-.356.09-.563.031-.081.06-.145.08-.182l.012-.022a.75.75 0 1 0-1.299-.752z'
-																transform='translate(448 544)'
-															></path>
-															<path
-																d='M9.557 11.659c.038-.018.09-.04.15-.064.207-.077.408-.112.562-.092.08.01.143.034.198.077l.041.036a.75.75 0 0 0 1.06-1.06 1.881 1.881 0 0 0-1.103-.54c-.435-.058-.867.018-1.284.175-.189.07-.336.143-.433.2a.75.75 0 0 0 .624 1.356l.066-.027.12-.061z'
-																transform='translate(448 544)'
-															></path>
-															<path
-																d='m13.463 15.142-.04-.044-3.574-4.192c-.599-.703.355-1.656 1.058-1.057l4.191 3.574.044.04c.058.059.122.137.182.24.249.425.249.96-.154 1.41l-.057.057c-.45.403-.986.403-1.411.154a1.182 1.182 0 0 1-.24-.182zm.617-.616.444-.444a.31.31 0 0 0-.063-.052c-.093-.055-.263-.055-.35.024l.208.232.207-.206.006.007-.22.257-.026-.024.033-.034.025.027-.257.22-.007-.007zm-.027-.415c-.078.088-.078.257-.023.35a.31.31 0 0 0 .051.063l.205-.204-.233-.209z'
-																transform='translate(448 544)'
-															></path>
-														</g>
-													</g>
-												</svg>
-											</span>
-											<input
-												type='text'
-												id='menu_search_odinbook'
-												name='menu_search_odinbook'
-												minLength={1}
-												maxLength={512}
-												value={searchOdinbookInput}
-												onChange={(e) => setSearchOdinbookInput(e.target.value)}
-												placeholder='Search Odinbook'
-											/>
-										</label>
-									</div>
 									<h3>Social</h3>
 									<div className={styles.social}>
 										<ul>
