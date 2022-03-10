aaeefec66abff455feb07b9d9c47c0c601714871
mrjackmaster <jacektrg+github@gmail.com>
2022-03-10 21:02:18 +0100

    Moving logOut function in. Removing inactive links. Cleanup

--- src/components/menus/AccountMenu.tsx
+++ src/components/menus/AccountMenu.tsx
@@ -1,14 +1,23 @@
 import { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { UserContext } from '../hooks/UserContext';
+import { axiosGet } from '../utils/axiosFunctions';
 import styles from '../../styles/menus/AccountMenu.module.scss';
 
-type Props = {
-	logOut: Function;
-};
+const AccountMenu = () => {
+	const { user, setUser } = useContext(UserContext);
 
-const AccountMenu: React.FC<Props> = ({ logOut }) => {
-	const { user } = useContext(UserContext);
+	const navigate = useNavigate();
+
+	const handleLogOut = async () => {
+		try {
+			await axiosGet('/api/log-out');
+			setUser(null);
+			navigate('/');
+		} catch (error: any) {
+			console.error(error);
+		}
+	};
 
 	return (
 		<div className={styles.menu_account}>
@@ -29,36 +38,33 @@ const AccountMenu: React.FC<Props> = ({ logOut }) => {
 				<h4>Give feedback</h4>
 			</Link>
 			<hr />
-			<Link to='settings-privacy' className={styles.settings_privacy_link}>
+			<Link to='/' className={styles.settings_privacy_link}>
 				<div className={styles.icon}>
 					<span></span>
 				</div>
 				<h4>{'Settings & privacy'}</h4>
 				<span className={styles.arrows}></span>
 			</Link>
-			<Link to='help-support' className={styles.help_support_link}>
+			<Link to='/' className={styles.help_support_link}>
 				<div className={styles.icon}>
 					<span></span>
 				</div>
 				<h4>{'Help & support'}</h4>
 				<span className={styles.arrows}></span>
 			</Link>
-			<Link
-				to='display-accesibility'
-				className={styles.display_accesibility_link}
-			>
+			<Link to='/' className={styles.display_accesibility_link}>
 				<div className={styles.icon}>
 					<span></span>
 				</div>
 				<h4>{'Display & accessibility'}</h4>
 				<span className={styles.arrows}></span>
 			</Link>
-			<Link to='/' onClick={() => logOut()} className={styles.log_out_btn}>
+			<div onClick={() => handleLogOut()} className={styles.log_out_btn}>
 				<div className={styles.icon}>
 					<span></span>
 				</div>
 				<h4>Log Out</h4>
-			</Link>
+			</div>
 		</div>
 	);
 };
