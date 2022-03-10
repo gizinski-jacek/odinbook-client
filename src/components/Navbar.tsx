602977ff09bb36bbb949639bec1f6379c92bed4d
mrjackmaster <jacektrg+github@gmail.com>
2022-03-10 15:42:43 +0100

    Minor renaming. Fetching request in navbar and displaying them. Replacing h2 tags with h3.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,8 +15,7 @@ const Navbar = () => {
 	const unreadRef = useRef(null);
 	const searchRef = useRef(null);
 
-	const [showNotificationsRequests, setShowNotificationsRequests] =
-		useState<User[]>();
+	const [showNotifsReqData, setShowNotifsReqData] = useState<User[]>();
 	const [showMenuContainer, setShowMenuContainer] = useState(false);
 	const [showMainMenu, setShowMainMenu] = useState(false);
 	const [showMessengerMenu, setShowMessengerMenu] = useState(false);
@@ -28,15 +27,15 @@ const Navbar = () => {
 	const [showResults, setShowResults] = useState(false);
 	const [showAllNotifications, setShowAllNotifications] = useState(true);
 
-	// useEffect(() => {
-	// 	(async () => {
-	// 		try {
-	// 			setShowNotificationsRequests(await axiosGet('/api/users/requests'));
-	// 		} catch (error: any) {
-	// 			console.error(error);
-	// 		}
-	// 	})();
-	// }, []);
+	useEffect(() => {
+		(async () => {
+			try {
+				setShowNotifsReqData(await axiosGet('/api/users/requests'));
+			} catch (error: any) {
+				console.error(error);
+			}
+		})();
+	}, []);
 
 	const handleLogOut = async () => {
 		try {
@@ -125,18 +124,15 @@ const Navbar = () => {
 		}
 	};
 
-	const notificationsRequestsDisplay = showNotificationsRequests?.map(
-		(request) => {
-			return;
-			// return (
-			// 	<RequestWrapper
-			// 		acceptRequest={request}
-			// 		declineRequest={request}
-			// 		key={request._id}
-			// 	/>
-			// );
-		}
-	);
+	const notificationsRequestsDisplay = showNotifsReqData?.map((request) => {
+		return (
+			<RequestWrapper
+				key={request._id}
+				request={request}
+				setData={setShowNotifsReqData}
+			/>
+		);
+	});
 
 	const handleSearch = async (
 		e: React.FormEvent<HTMLFormElement>,
@@ -372,6 +368,12 @@ const Navbar = () => {
 								<path d='M7.847 23.488C9.207 23.488 11.443 23.363 14.467 22.806 13.944 24.228 12.581 25.247 10.98 25.247 9.649 25.247 8.483 24.542 7.825 23.488L7.847 23.488ZM24.923 15.73C25.17 17.002 24.278 18.127 22.27 19.076 21.17 19.595 18.724 20.583 14.684 21.369 11.568 21.974 9.285 22.113 7.848 22.113 7.421 22.113 7.068 22.101 6.79 22.085 4.574 21.958 3.324 21.248 3.077 19.976 2.702 18.049 3.295 17.305 4.278 16.073L4.537 15.748C5.2 14.907 5.459 14.081 5.035 11.902 4.086 7.022 6.284 3.687 11.064 2.753 15.846 1.83 19.134 4.096 20.083 8.977 20.506 11.156 21.056 11.824 21.986 12.355L21.986 12.356 22.348 12.561C23.72 13.335 24.548 13.802 24.923 15.73Z'></path>
 							</svg>
 						</span>
+						{notificationsRequestsDisplay &&
+						notificationsRequestsDisplay?.length > 0 ? (
+							<span className={styles.notification_count}>
+								<h5> {notificationsRequestsDisplay?.length} </h5>
+							</span>
+						) : null}
 					</li>
 					<li className='messenger' onClick={(e) => toggleMenu(e)}>
 						<span
@@ -497,7 +499,7 @@ const Navbar = () => {
 					{showNotificationsMenu ? (
 						<div className={styles.menu_notifications}>
 							<div className={styles.top}>
-								<h2>Notifications</h2>
+								<h3>Notifications</h3>
 								<span className={styles.options_toggle}>
 									<svg viewBox='0 0 20 20' width='20' height='20'>
 										<g transform='translate(-446 -350)'>
@@ -538,7 +540,7 @@ const Navbar = () => {
 					{showMessengerMenu ? (
 						<div className={styles.menu_messenger}>
 							<div className={styles.top}>
-								<h2>Messenger</h2>
+								<h3>Messenger</h3>
 								<span className={styles.options_toggle}>
 									<svg viewBox='0 0 20 20' width='20' height='20'>
 										<g transform='translate(-446 -350)'>
@@ -598,7 +600,7 @@ const Navbar = () => {
 					) : null}
 					{showMainMenu ? (
 						<div className={styles.menu_main}>
-							<h2>Menu</h2>
+							<h3>Menu</h3>
 							<div className={styles.both_sides}>
 								<div className={styles.left_side}>
 									<h3>Social</h3>
