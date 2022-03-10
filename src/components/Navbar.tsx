8ecfc4cdae2bae8aa349f4632b16b50eaa52240c
mrjackmaster <jacektrg+github@gmail.com>
2022-03-10 21:35:47 +0100

    Refactoring navbar menu's. Using ref's on button opening each menu with value provided from them to open correct menu. Moving each menu to its own component. Moving log out function to relevant component. Adding tabIndex of 0 to make list items selectable by tabbing.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,107 +1,71 @@
-// @ts-nocheck
-
-import { useContext, useRef, useState } from 'react';
-import { Link, NavLink, useNavigate } from 'react-router-dom';
+import { useContext, useEffect, useRef, useState } from 'react';
+import { Link, NavLink, useLocation } from 'react-router-dom';
 import { UserContext } from './hooks/UserContext';
 import type { PostFull } from '../myTypes';
 import { axiosGet } from './utils/axiosFunctions';
-import styles from '../styles/Navbar.module.scss';
-import MainMenu from './menus/MainMenu';
-import MessengerMenu from './menus/MessengerMenu';
-import NotificationsMenu from './menus/NotificationsMenu';
 import AccountMenu from './menus/AccountMenu';
+import NotificationsMenu from './menus/NotificationsMenu';
+import MessengerMenu from './menus/MessengerMenu';
+import MainMenu from './menus/MainMenu';
+import styles from '../styles/Navbar.module.scss';
 
 const Navbar = () => {
-	const { user, setUser } = useContext(UserContext);
+	const { user } = useContext(UserContext);
 
-	const navigate = useNavigate();
+	const location = useLocation();
 
 	const searchRef = useRef(null);
 	const menuContainerRef = useRef(null);
-	const menuRef = useRef(null);
-	console.log(menuRef);
 
-	const [showMenuContainer, setShowMenuContainer] = useState(true);
-	const [showAccountMenu, setShowAccountMenu] = useState(true);
-	const [showNotificationsMenu, setShowNotificationsMenu] = useState(false);
-	const [showMainMenu, setShowMainMenu] = useState(false);
-	const [showMessengerMenu, setShowMessengerMenu] = useState(false);
+	const [openMenuContainer, setOpenMenuContainer] = useState(false);
+	const [openMenu, setOpenMenu] = useState(0);
 
 	const [searchInput, setSearchInput] = useState('');
-	const [searchData, setSearchData] = useState<PostFull[] | null>();
+	const [searchData, setSearchData] = useState<PostFull[]>([]);
 	const [showResults, setShowResults] = useState(false);
+	const [notificationAlert, setNotificationAlert] = useState(0);
 
-	const handleLogOut = async () => {
-		try {
-			await axiosGet('/api/log-out');
-			setUser(null);
-			navigate('/');
-		} catch (error: any) {
-			console.error(error);
-		}
-	};
+	useEffect(() => {
+		(async () => {
+			try {
+				const resData = await axiosGet('/api/users/requests');
+				setNotificationAlert(resData.length);
+			} catch (error: any) {
+				console.error(error);
+			}
+		})();
+	}, []);
 
-	const toggleMenu = (e: React.MouseEvent<HTMLLIElement>) => {
+	useEffect(() => {
+		setOpenMenuContainer(false);
+		setOpenMenu(0);
+	}, [location]);
+
+	const toggleMenu = (
+		e: React.MouseEvent<HTMLLIElement>,
+		menuNumber: number
+	) => {
 		e.stopPropagation();
-		if (!showMenuContainer) {
-			setShowMenuContainer(true);
+		if (!openMenuContainer) {
+			setOpenMenuContainer(true);
+			setOpenMenu(menuNumber);
 			document.addEventListener('click', closeMenuContainer);
+			return;
 		}
-		switch (e.currentTarget.className) {
-			case 'account':
-				if (showAccountMenu) {
-					setShowMenuContainer(false);
-				}
-				setShowMainMenu(false);
-				setShowMessengerMenu(false);
-				setShowNotificationsMenu(false);
-				setShowAccountMenu(!showAccountMenu);
-				break;
-			case 'notifications':
-				if (showNotificationsMenu) {
-					setShowMenuContainer(false);
-				}
-				setShowMainMenu(false);
-				setShowMessengerMenu(false);
-				setShowNotificationsMenu(!showNotificationsMenu);
-				setShowAccountMenu(false);
-				break;
-			case 'messenger':
-				if (showMessengerMenu) {
-					setShowMenuContainer(false);
-				}
-				setShowMainMenu(false);
-				setShowMessengerMenu(!showMessengerMenu);
-				setShowNotificationsMenu(false);
-				setShowAccountMenu(false);
-				break;
-			case 'mainMenu':
-				if (showMainMenu) {
-					setShowMenuContainer(false);
-				}
-				setShowMainMenu(!showMainMenu);
-				setShowMessengerMenu(false);
-				setShowNotificationsMenu(false);
-				setShowAccountMenu(false);
-				break;
-			default:
-				setShowMainMenu(false);
-				setShowMessengerMenu(false);
-				setShowNotificationsMenu(false);
-				setShowAccountMenu(false);
-				setShowMenuContainer(false);
-				break;
+		if (openMenuContainer && openMenu === menuNumber) {
+			setOpenMenuContainer(false);
+			setOpenMenu(0);
+			document.removeEventListener('click', closeMenuContainer);
+			return;
 		}
+		setOpenMenu(menuNumber);
 	};
 
 	const closeMenuContainer = (e: any) => {
 		e.stopPropagation();
 		if (menuContainerRef.current !== e.target) {
-			setShowMainMenu(false);
-			setShowMessengerMenu(false);
-			setShowNotificationsMenu(false);
-			setShowAccountMenu(false);
-			setShowMenuContainer(false);
+			setOpenMenuContainer(false);
+			setOpenMenu(0);
 			document.removeEventListener('click', closeMenuContainer);
 		}
 	};
@@ -123,7 +87,7 @@ const Navbar = () => {
 	const clearSearch = (e: React.MouseEvent<HTMLElement>) => {
 		e.stopPropagation();
 		setSearchInput('');
-		setSearchData(null);
+		setSearchData([]);
 		setShowResults(false);
 	};
 
@@ -221,7 +185,7 @@ const Navbar = () => {
 							/>
 							<div
 								style={{
-									visibility: searchInput || searchData ? 'visible' : 'hidden',
+									visibility: searchInput || showResults ? 'visible' : 'hidden',
 								}}
 								className={styles.clear_btn}
 								onClick={(e) => clearSearch(e)}
@@ -230,7 +194,7 @@ const Navbar = () => {
 							</div>
 						</label>
 					</form>
-					{showResults && searchDisplay && searchDisplay.length > 0 ? (
+					{showResults && searchDisplay.length > 0 ? (
 						<div ref={searchRef} className={styles.search_results_container}>
 							<ul>{searchDisplay}</ul>
 						</div>
@@ -291,7 +255,7 @@ const Navbar = () => {
 					</li>
 					<li>
 						<NavLink
-							to='/groups'
+							to='/'
 							className={({ isActive }) => (isActive ? styles.isActive : '')}
 						>
 							<span>
@@ -318,48 +282,59 @@ const Navbar = () => {
 			</div>
 			<div className={styles.right}>
 				<ul className={styles.user_controls}>
-					<li className={styles.account} onClick={(e) => toggleMenu(e)}>
+					<li
+						id='menu-1'
+						tabIndex={0}
+						className={styles.account}
+						onClick={(e) => toggleMenu(e, 1)}
+					>
 						<span
-							className={`${styles.icon} ${
-								showAccountMenu ? 'btn-active' : ''
-							}`}
+							className={`${styles.icon} ${openMenu === 1 ? 'btn-active' : ''}`}
 						>
 							<svg viewBox='0 0 20 20' width='20' height='20'>
 								<path d='M10 14a1 1 0 0 1-.755-.349L5.329 9.182a1.367 1.367 0 0 1-.205-1.46A1.184 1.184 0 0 1 6.2 7h7.6a1.18 1.18 0 0 1 1.074.721 1.357 1.357 0 0 1-.2 1.457l-3.918 4.473A1 1 0 0 1 10 14z'></path>
 							</svg>
 						</span>
 					</li>
-					<li className={styles.notifications} onClick={(e) => toggleMenu(e)}>
+					<li
+						id='menu-2'
+						tabIndex={0}
+						className={styles.notifications}
+						onClick={(e) => toggleMenu(e, 2)}
+					>
 						<span
-							className={`${styles.icon} ${
-								showNotificationsMenu ? 'btn-active' : ''
-							}`}
+							className={`${styles.icon} ${openMenu === 2 ? 'btn-active' : ''}`}
 						>
 							<svg viewBox='0 0 28 28' height='20' width='20'>
 								<path d='M7.847 23.488C9.207 23.488 11.443 23.363 14.467 22.806 13.944 24.228 12.581 25.247 10.98 25.247 9.649 25.247 8.483 24.542 7.825 23.488L7.847 23.488ZM24.923 15.73C25.17 17.002 24.278 18.127 22.27 19.076 21.17 19.595 18.724 20.583 14.684 21.369 11.568 21.974 9.285 22.113 7.848 22.113 7.421 22.113 7.068 22.101 6.79 22.085 4.574 21.958 3.324 21.248 3.077 19.976 2.702 18.049 3.295 17.305 4.278 16.073L4.537 15.748C5.2 14.907 5.459 14.081 5.035 11.902 4.086 7.022 6.284 3.687 11.064 2.753 15.846 1.83 19.134 4.096 20.083 8.977 20.506 11.156 21.056 11.824 21.986 12.355L21.986 12.356 22.348 12.561C23.72 13.335 24.548 13.802 24.923 15.73Z'></path>
 							</svg>
 						</span>
-						{/* {notificationsRequestsDisplay &&
-						notificationsRequestsDisplay?.length > 0 ? (
-							<span className={styles.notification_count}>
-								<h5> {notificationsRequestsDisplay?.length} </h5>
-							</span>
-						) : null} */}
+						{notificationAlert > 0 ? (
+							<span className={styles.notification_alert}></span>
+						) : null}
 					</li>
-					<li className={styles.messenger} onClick={(e) => toggleMenu(e)}>
+					<li
+						id='menu-3'
+						tabIndex={0}
+						className={styles.messenger}
+						onClick={(e) => toggleMenu(e, 3)}
+					>
 						<span
-							className={`${styles.icon} ${
-								showMessengerMenu ? 'btn-active' : ''
-							}`}
+							className={`${styles.icon} ${openMenu === 3 ? 'btn-active' : ''}`}
 						>
 							<svg viewBox='0 0 28 28' height='20' width='20'>
 								<path d='M14 2.042c6.76 0 12 4.952 12 11.64S20.76 25.322 14 25.322a13.091 13.091 0 0 1-3.474-.461.956 .956 0 0 0-.641.047L7.5 25.959a.961.961 0 0 1-1.348-.849l-.065-2.134a.957.957 0 0 0-.322-.684A11.389 11.389 0 0 1 2 13.682C2 6.994 7.24 2.042 14 2.042ZM6.794 17.086a.57.57 0 0 0 .827.758l3.786-2.874a.722.722 0 0 1 .868 0l2.8 2.1a1.8 1.8 0 0 0 2.6-.481l3.525-5.592a.57.57 0 0 0-.827-.758l-3.786 2.874a.722.722 0 0 1-.868 0l-2.8-2.1a1.8 1.8 0 0 0-2.6.481Z'></path>
 							</svg>
 						</span>
 					</li>
-					<li className={styles.main_menu} onClick={(e) => toggleMenu(e)}>
+					<li
+						id='menu-4'
+						tabIndex={0}
+						className={styles.main_menu}
+						onClick={(e) => toggleMenu(e, 4)}
+					>
 						<span
-							className={`${styles.icon} ${showMainMenu ? 'btn-active' : ''}`}
+							className={`${styles.icon} ${openMenu === 4 ? 'btn-active' : ''}`}
 						>
 							<svg viewBox='0 0 44 44' width='20' height='20'>
 								<circle cx='7' cy='7' r='6'></circle>
@@ -381,13 +356,13 @@ const Navbar = () => {
 								isActive ? `${styles.me_link} btn-active` : styles.me_link
 							}
 						>
-							<h4>{user.first_name}</h4>
 							<div className='profile-pic-style'>
 								<img
 									src='/placeholder_profile_pic.png'
 									alt='User profile pic'
 								/>
 							</div>
+							<h4>{user.first_name}</h4>
 						</NavLink>
 					</li>
 					<li>
@@ -403,18 +378,16 @@ const Navbar = () => {
 						</NavLink>
 					</li>
 				</ul>
-				{showMenuContainer ? (
+				{!openMenuContainer ? null : (
 					<div ref={menuContainerRef} className={styles.menu_container}>
-						{showAccountMenu ? (
-							<AccountMenu logOut={handleLogOut} menuRef={menuRef} />
+						{openMenu === 1 ? <AccountMenu /> : null}
+						{openMenu === 2 ? (
+							<NotificationsMenu setNotificationAlert={setNotificationAlert} />
 						) : null}
-						{showNotificationsMenu ? (
-							<NotificationsMenu menuRef={menuRef} />
-						) : null}
-						{showMessengerMenu ? <MessengerMenu menuRef={menuRef} /> : null}
-						{showMainMenu ? <MainMenu menuRef={menuRef} /> : null}
+						{openMenu === 3 ? <MessengerMenu /> : null}
+						{openMenu === 4 ? <MainMenu /> : null}
 					</div>
-				) : null}
+				)}
 			</div>
 		</nav>
 	);
