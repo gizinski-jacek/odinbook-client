e9e06166e6174cef2afd10e71605056cf5baf1f5
mrjackmaster <jacektrg+github@gmail.com>
2022-03-12 00:45:15 +0100

    Moving in functions handling requests accept/cancel. Adding remove friend function. Passing them down to wrappers.

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -1,11 +1,14 @@
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
+import { UserContext } from './hooks/UserContext';
 import type { User } from '../myTypes';
-import { axiosGet } from './utils/axiosFunctions';
+import { axiosGet, axiosPut } from './utils/axiosFunctions';
 import RequestWrapper from './utils/RequestWrapper';
 import FriendWrapper from './utils/FriendWrapper';
 import styles from '../styles/Contacts.module.scss';
 
 const Contacts = () => {
+	const { user } = useContext(UserContext);
+
 	const [requestsData, setRequestsData] = useState<User[]>([]);
 	const [friendsData, setFriendsData] = useState<User[]>([]);
 	const [conversationsData, setConversationsData] = useState<User[]>([]);
@@ -13,13 +16,63 @@ const Contacts = () => {
 	useEffect(() => {
 		(async () => {
 			try {
-				setRequestsData(await axiosGet('/api/users/requests'));
-				setFriendsData(await axiosGet('/api/users/friends'));
+				const resData = await axiosGet('/api/users/contacts');
+				setRequestsData(resData.incoming_friend_requests);
+				setFriendsData(resData.friend_list);
 			} catch (error: any) {
 				console.error(error);
 			}
 		})();
-	}, []);
+	}, [user]);
+
+	const handleAcceptRequest = async (
+		e: React.MouseEvent<HTMLButtonElement>,
+		userId: string
+	) => {
+		e.stopPropagation();
+		try {
+			const resData = await axiosPut(`/api/users/friends/accept`, {
+				userId,
+			});
+			const data = resData.find((u: User) => u._id === user._id);
+			setRequestsData(data.incoming_friend_requests);
+			setFriendsData(data.friend_list);
+		} catch (error: any) {
+			console.error(error);
+		}
+	};
+
+	const handleCancelRequest = async (
+		e: React.MouseEvent<HTMLButtonElement>,
+		userId: string
+	) => {
+		e.stopPropagation();
+		try {
+			const resData = await axiosPut(`/api/users/friends/cancel`, {
+				userId,
+			});
+			const data = resData.find((u: User) => u._id === user._id);
+			setRequestsData(data.incoming_friend_requests);
+			setFriendsData(data.friend_list);
+		} catch (error: any) {
+			console.error(error);
+		}
+	};
+
+	const handleRemoveFriend = async (
+		e: React.MouseEvent<HTMLButtonElement>,
+		userId: string
+	) => {
+		e.stopPropagation();
+		try {
+			const resData = await axiosPut(`/api/users/friends/remove`, { userId });
+			const data = resData.find((u: User) => u._id === user._id);
+			setRequestsData(data.incoming_friend_requests);
+			setFriendsData(data.friend_list);
+		} catch (error: any) {
+			console.error(error);
+		}
+	};
 
 	const openChat = async () => {
 		try {
@@ -33,15 +86,21 @@ const Contacts = () => {
 		return (
 			<RequestWrapper
 				key={request._id}
+				handleAccept={handleAcceptRequest}
+				handleCancel={handleCancelRequest}
 				request={request}
-				setData={setRequestsData}
 			/>
 		);
 	});
 
 	const friendsDisplay = friendsData?.map((friend) => {
 		return (
-			<FriendWrapper key={friend._id} friend={friend} openChat={openChat} />
+			<FriendWrapper
+				key={friend._id}
+				handleRemove={handleRemoveFriend}
+				friend={friend}
+				openChat={openChat}
+			/>
 		);
 	});
 
