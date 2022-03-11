6d86bd7107671639c60fae4663b8ea6d589c3530
mrjackmaster <jacektrg+github@gmail.com>
2022-03-12 00:42:40 +0100

    Moving in functions for accepting/canceling requests. Some cleanup.

--- src/components/menus/NotificationsMenu.tsx
+++ src/components/menus/NotificationsMenu.tsx
@@ -1,6 +1,7 @@
-import { useEffect, useRef, useState } from 'react';
+import { useContext, useEffect, useRef, useState } from 'react';
+import { UserContext } from '../hooks/UserContext';
 import { User } from '../../myTypes';
-import { axiosGet } from '../utils/axiosFunctions';
+import { axiosGet, axiosPut } from '../utils/axiosFunctions';
 import RequestWrapper from '../utils/RequestWrapper';
 import styles from '../../styles/menus/NotificationsMenu.module.scss';
 
@@ -9,54 +10,82 @@ type Props = {
 };
 
 const NotificationsMenu: React.FC<Props> = ({ setNotificationAlert }) => {
+	const { user } = useContext(UserContext);
+
 	const allRef = useRef(null);
 	const unreadRef = useRef(null);
 
-	const [showAllNotifications, setShowAllNotifications] = useState(true);
-	const [notificationsRequestsData, setNotificationsRequestsData] = useState<
-		User[]
-	>([]);
+	const [showAll, setShowAll] = useState(true);
+	const [requestsData, setRequestsData] = useState<User[]>([]);
 
 	useEffect(() => {
 		(async () => {
 			try {
-				const data = await axiosGet('/api/users/requests');
-				setNotificationsRequestsData(data);
-				setNotificationAlert(data.length);
+				const resData = await axiosGet('/api/users/contacts');
+				setRequestsData(resData.incoming_friend_requests);
+				setNotificationAlert(resData.incoming_friend_requests.length);
 			} catch (error: any) {
 				console.error(error);
 			}
 		})();
 	}, [setNotificationAlert]);
 
+	const handleAcceptRequest = async (
+		e: React.MouseEvent<HTMLButtonElement>,
+		userId: string
+	) => {
+		e.stopPropagation();
+		try {
+			const resData = await axiosPut(`/api/users/friends/accept`, {
+				userId,
+			});
+			const data = resData.find((u: User) => u._id === user._id);
+			setRequestsData(data.incoming_friend_requests);
+			setNotificationAlert(data.incoming_friend_requests.length);
+		} catch (error: any) {
+			console.error(error);
+		}
+	};
+
+	const handleCancelRequest = async (
+		e: React.MouseEvent<HTMLButtonElement>,
+		userId: string
+	) => {
+		e.stopPropagation();
+		try {
+			const resData = await axiosPut(`/api/users/friends/cancel`, {
+				userId,
+			});
+			const data = resData.find((u: User) => u._id === user._id);
+			setRequestsData(data.incoming_friend_requests);
+			setNotificationAlert(data.incoming_friend_requests.length);
+		} catch (error: any) {
+			console.error(error);
+		}
+	};
+
 	const changeNotificationList = (e: any) => {
 		e.stopPropagation();
 		if (e.target === allRef.current) {
 			// fetch all notifications
-			setShowAllNotifications(true);
+			setShowAll(true);
 		}
 		if (e.target === unreadRef.current) {
 			// fetch unread notifications
-			setShowAllNotifications(false);
+			setShowAll(false);
 		}
 	};
 
-	const handleSetData = (data: User[]) => {
-		setNotificationsRequestsData(data);
-		setNotificationAlert(data.length);
-	};
-
-	const notificationsRequestsDisplay = notificationsRequestsData?.map(
-		(request) => {
-			return (
-				<RequestWrapper
-					key={request._id}
-					request={request}
-					setData={handleSetData}
-				/>
-			);
-		}
-	);
+	const notificationsRequestsDisplay = requestsData?.map((request) => {
+		return (
+			<RequestWrapper
+				key={request._id}
+				handleAccept={handleAcceptRequest}
+				handleCancel={handleCancelRequest}
+				request={request}
+			/>
+		);
+	});
 
 	return (
 		<div className={styles.menu_notifications}>
@@ -73,14 +102,14 @@ const NotificationsMenu: React.FC<Props> = ({ setNotificationAlert }) => {
 			<div className={styles.controls}>
 				<div
 					ref={allRef}
-					className={`btn-default ${showAllNotifications ? 'btn-active' : ''}`}
+					className={`btn-default ${showAll ? 'btn-active' : ''}`}
 					onClick={changeNotificationList}
 				>
 					All
 				</div>
 				<div
 					ref={unreadRef}
-					className={`btn-default ${showAllNotifications ? '' : 'btn-active'}`}
+					className={`btn-default ${showAll ? '' : 'btn-active'}`}
 					onClick={changeNotificationList}
 				>
 					Unread
