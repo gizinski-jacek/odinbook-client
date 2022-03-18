d6d8a8f6e97e4a2962e9e249151b9c56126937ff
mrjackmaster <jacektrg+github@gmail.com>
2022-03-18 20:56:41 +0100

    Removing hobbies section and related functions. Handling bio change, file change. Handling sending bio data and file data with axios to api for update. Added remove btn to selected file. Displaying either selected file, user picture or placeholder pic.

--- src/components/EditProfileModal.tsx
+++ src/components/EditProfileModal.tsx
@@ -1,4 +1,5 @@
-import { useRef, useState } from 'react';
+import { useContext, useRef, useState } from 'react';
+import { UserContext } from './hooks/UserContext';
 import { User } from '../myTypes';
 import { axiosPut } from './utils/axiosFunctions';
 import styles from '../styles/EditProfileModal.module.scss';
@@ -10,47 +11,69 @@ type Props = {
 };
 
 const EditProfileModal: React.FC<Props> = ({ closeModal, setData, data }) => {
+	const { user } = useContext(UserContext);
+	const pictureRef = useRef<HTMLInputElement>(null);
+
 	const [errors, setErrors] = useState<{ msg: string }[]>();
-	const [formData, setFormData] = useState(data);
-
-	const profilePicRef = useRef<HTMLInputElement>(null);
-
 	const [bioForm, setBioForm] = useState(false);
-	const [hobbiesForm, setHobbiesForm] = useState(false);
+	const [bioInput, setBioInput] = useState(data.bio ? data.bio : '');
+	const [pictureData, setPictureData] = useState<{
+		preview: string;
+		data: {};
+	} | null>(null);
 
-	const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-		const { name, value } = e.target;
-		setFormData((prevState) => ({
-			...prevState,
-			[name]: value,
-		}));
-	};
-
-	const handleSubmit = async (
-		e: React.FormEvent<HTMLFormElement>,
-		data: User
-	) => {
-		e.preventDefault();
-		try {
-			setData(await axiosPut(`/api/users/${data._id}`, data));
-		} catch (error: any) {
-			console.error(error);
-		}
-	};
-
-	const addPicture = (e: React.MouseEvent<HTMLDivElement>) => {
+	const clickSelectFile = (e: React.MouseEvent<HTMLElement>) => {
 		e.stopPropagation();
-		profilePicRef.current?.click();
+		pictureRef.current?.click();
 	};
 
-	const toggleBioForm = (e: React.MouseEvent<HTMLDivElement>) => {
+	const toggleBioForm = (e: React.MouseEvent<HTMLButtonElement>) => {
 		e.stopPropagation();
 		setBioForm((prevState) => !prevState);
 	};
 
-	const toggleHobbiesForm = (e: React.MouseEvent<HTMLDivElement>) => {
+	const handleAddFile = (e: any) => {
 		e.stopPropagation();
-		setHobbiesForm((prevState) => !prevState);
+		const file = e.target.files[0];
+		if (file.size > 2000000) {
+			setErrors([{ msg: 'File too big. Max. size 2mb' }]);
+			return;
+		}
+		setPictureData({
+			preview: URL.createObjectURL(file),
+			data: file,
+		});
+	};
+
+	const handleRemoveFile = (e: React.MouseEvent<HTMLButtonElement>) => {
+		e.stopPropagation();
+		setPictureData(null);
+	};
+
+	const handleSubmit = async (
+		e: React.FormEvent<HTMLFormElement>,
+		bio: string,
+		profile_picture: any
+	) => {
+		e.preventDefault();
+		try {
+			const newData = new FormData();
+			newData.append('bio', bio);
+			newData.append('profile_picture', profile_picture);
+			setData(await axiosPut(`/api/users/${user._id}`, newData));
+			closeModal(e);
+		} catch (error: any) {
+			if (!Array.isArray(error.response.data)) {
+				if (typeof error.response.data === 'object') {
+					setErrors([error.response.data]);
+				}
+				if (typeof error.response.data === 'string') {
+					setErrors([{ msg: error.response.data }]);
+				}
+			} else {
+				setErrors(error.response.data);
+			}
+		}
 	};
 
 	const errorsDisplay = errors?.map((error, index) => {
@@ -69,34 +92,60 @@ const EditProfileModal: React.FC<Props> = ({ closeModal, setData, data }) => {
 					<div className={styles.title}>
 						<h3>Edit profile</h3>
 					</div>
-					<div className={styles.close_btn} onClick={(e) => closeModal(e)}>
+					<button
+						type='button'
+						className={styles.close_btn}
+						onClick={(e) => closeModal(e)}
+					>
 						<span></span>
-					</div>
+					</button>
 				</div>
 				<hr />
 				{errorsDisplay ? <ul className='error-list'>{errorsDisplay}</ul> : null}
 				<div className={styles.body}>
-					<form onSubmit={(e) => handleSubmit(e, formData)}>
+					<form
+						encType='multipart/form-data'
+						onSubmit={(e) => handleSubmit(e, bioInput, pictureData?.data)}
+					>
 						<div className={styles.edit_profile_picture}>
 							<div className={styles.section_name}>
 								<h3>Profile picture</h3>
-								<div
-									className={`btn-default btn-active ${styles.add_btn}`}
-									onClick={addPicture}
-								>
-									Edit
+								<div className={styles.controls}>
+									{pictureData ? (
+										<button
+											type='button'
+											className={`btn-default btn-danger ${styles.remove_btn}`}
+											onClick={handleRemoveFile}
+										>
+											Remove
+										</button>
+									) : null}
+									<button
+										type='button'
+										className={`btn-default btn-active ${styles.add_btn}`}
+										onClick={clickSelectFile}
+									>
+										Edit
+									</button>
+									<input
+										style={{ display: 'none' }}
+										ref={pictureRef}
+										type='file'
+										id='profile_picture'
+										name='profile_picture'
+										onChange={handleAddFile}
+									/>
 								</div>
-								<input
-									style={{ display: 'none' }}
-									ref={profilePicRef}
-									type='file'
-									name='test1'
-									id='test'
-								/>
 							</div>
-							<div className='profile-pic-style' onClick={addPicture}>
+							<div className='profile-pic-style' onClick={clickSelectFile}>
 								<img
-									src='/placeholder_profile_pic.png'
+									src={
+										pictureData?.preview
+											? pictureData.preview
+											: user.profile_picture
+											? `http://localhost:4000/${user.profile_picture}`
+											: '/placeholder_profile_pic.png'
+									}
 									alt='User profile pic'
 								/>
 							</div>
@@ -104,11 +153,14 @@ const EditProfileModal: React.FC<Props> = ({ closeModal, setData, data }) => {
 						<div className={styles.edit_bio}>
 							<div className={styles.section_name}>
 								<h3>Bio</h3>
-								<div
-									className={`btn-default btn-active ${styles.edit_btn}`}
-									onClick={toggleBioForm}
-								>
-									{bioForm ? 'Save' : 'Edit'}
+								<div className={styles.controls}>
+									<button
+										type='button'
+										className={`btn-default btn-active ${styles.edit_btn}`}
+										onClick={toggleBioForm}
+									>
+										{bioForm ? 'Save' : 'Edit'}
+									</button>
 								</div>
 							</div>
 							{bioForm ? (
@@ -117,47 +169,16 @@ const EditProfileModal: React.FC<Props> = ({ closeModal, setData, data }) => {
 										id='bio'
 										name='bio'
 										maxLength={512}
-										rows={3}
-										onChange={handleChange}
-										value={formData.bio}
-										required
+										rows={5}
+										onChange={(e) => setBioInput(e.target.value)}
+										value={bioInput}
 										placeholder='Describe yourself...'
+										autoFocus
 									/>
 								</label>
 							) : (
-								<p>{formData.bio}</p>
+								<p>{bioInput}</p>
 							)}
-						</div>
-						<div className={styles.edit_hobbies}>
-							<div className={styles.section_name}>
-								<h3>Hobbies</h3>
-								<div
-									className={`btn-default btn-active ${styles.edit_btn}`}
-									onClick={toggleHobbiesForm}
-								>
-									{hobbiesForm ? 'Save' : 'Edit'}
-								</div>
-							</div>
-							{hobbiesForm ? (
-								<>
-									<label>
-										<input type='checkbox' id='sports' name='sports' />
-										Sports
-									</label>
-									<label>
-										<input type='checkbox' id='music' name='music' />
-										Music
-									</label>
-									<label>
-										<input type='checkbox' id='movies' name='movies' />
-										Movies
-									</label>
-									<label>
-										<input type='checkbox' id='gaming' name='gaming' />
-										Gaming
-									</label>
-								</>
-							) : null}
 						</div>
 						<button type='submit' className='btn-default btn-form-submit'>
 							Save Your Profile Info
