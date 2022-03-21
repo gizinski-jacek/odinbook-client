4089ef340c7227e628b22f56a950cf7b08190cc2
mrjackmaster <jacektrg+github@gmail.com>
2022-03-22 00:37:16 +0100

    Removing link from profile picture. Adding showChat state and function handling open/close of chat. Importing Chat component.

--- src/components/utils/FriendWrapper.tsx
+++ src/components/utils/FriendWrapper.tsx
@@ -1,18 +1,21 @@
-import { useRef, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useContext, useRef, useState } from 'react';
 import type { User } from '../../myTypes';
 import styles from '../../styles/Friend.module.scss';
+import Chat from '../Chat';
+import { UserContext } from '../hooks/UserContext';
 
 type Props = {
 	handleRemove: Function;
-	openChat: Function;
 	friend: User;
 };
 
-const FriendWrapper: React.FC<Props> = ({ handleRemove, openChat, friend }) => {
+const FriendWrapper: React.FC<Props> = ({ handleRemove, friend }) => {
+	const { user } = useContext(UserContext);
+
 	const optionsRef = useRef<HTMLDivElement>(null);
 
 	const [showOptions, setShowOptions] = useState(false);
+	const [showChat, setShowChat] = useState(false);
 
 	const toggleOptions = (e: React.MouseEvent<HTMLSpanElement>) => {
 		e.stopPropagation();
@@ -28,9 +31,19 @@ const FriendWrapper: React.FC<Props> = ({ handleRemove, openChat, friend }) => {
 		}
 	};
 
+	const openChat = (e: React.MouseEvent<HTMLLIElement>) => {
+		e.stopPropagation();
+		setShowChat(true);
+	};
+
+	const closeChat = (e: React.MouseEvent<HTMLButtonElement>) => {
+		e.stopPropagation();
+		setShowChat(false);
+	};
+
 	return (
-		<li className={styles.friend} onClick={() => openChat()}>
-			<Link to={`/profile/${friend._id}`}>
+		<>
+			<li className={styles.friend} onClick={openChat}>
 				<div className='profile-pic-style'>
 					<img
 						src={
@@ -41,32 +54,35 @@ const FriendWrapper: React.FC<Props> = ({ handleRemove, openChat, friend }) => {
 						alt='User profile pic'
 					/>
 				</div>
-			</Link>
-			<div>
 				<div>
-					{friend.first_name} {friend.last_name}
+					<div>
+						{friend.first_name} {friend.last_name}
+					</div>
 				</div>
-			</div>
-			<div className={styles.right}>
-				<span className={styles.options_toggle} onClick={toggleOptions}>
-					<svg viewBox='0 0 20 20' width='20' height='20'>
-						<g transform='translate(-446 -350)'>
-							<path d='M458 360a2 2 0 1 1-4 0 2 2 0 0 1 4 0m6 0a2 2 0 1 1-4 0 2 2 0 0 1 4 0m-12 0a2 2 0 1 1-4 0 2 2 0 0 1 4 0'></path>
-						</g>
-					</svg>
-				</span>
-				{showOptions ? (
-					<span ref={optionsRef} className={styles.options_menu}>
-						<div
-							className={styles.remove_btn}
-							onClick={(e) => handleRemove(e, friend._id)}
-						>
-							Remove friend
-						</div>
+				<div className={styles.right}>
+					<span className={styles.options_toggle} onClick={toggleOptions}>
+						<svg viewBox='0 0 20 20' width='20' height='20'>
+							<g transform='translate(-446 -350)'>
+								<path d='M458 360a2 2 0 1 1-4 0 2 2 0 0 1 4 0m6 0a2 2 0 1 1-4 0 2 2 0 0 1 4 0m-12 0a2 2 0 1 1-4 0 2 2 0 0 1 4 0'></path>
+							</g>
+						</svg>
 					</span>
-				) : null}
-			</div>
-		</li>
+					{showOptions && (
+						<span ref={optionsRef} className={styles.options_menu}>
+							<div
+								className={styles.remove_btn}
+								onClick={(e) => handleRemove(e, friend._id)}
+							>
+								Remove friend
+							</div>
+						</span>
+					)}
+				</div>
+			</li>
+			{showChat && (
+				<Chat closeChat={closeChat} sender={user} recipient={friend} />
+			)}
+		</>
 	);
 };
 
