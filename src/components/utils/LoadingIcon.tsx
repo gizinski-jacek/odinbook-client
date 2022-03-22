da9a09463809d6c0de0da9bec0de75d9cf13aa05
mrjackmaster <jacektrg+github@gmail.com>
2022-03-22 23:23:56 +0100

    Moving styling to inline. Adding text prop for passing custom message, or defaulting it to 'Loading'.

--- src/components/utils/LoadingIcon.tsx
+++ src/components/utils/LoadingIcon.tsx
@@ -1,18 +1,35 @@
-const LoadingIcon = () => {
-	const styles = {
-		loading_icon: {
-			display: 'flex',
-			justifyContent: 'center',
-			alignItems: 'center',
-			flex: 1,
-		},
-		svg: { width: '256px', height: '256px' },
-	};
+type Props = {
+	text?: string;
+};
 
+const LoadingIcon: React.FC<Props> = ({ text = 'Loading' }) => {
 	return (
-		<div style={styles.loading_icon}>
-			<h3 style={{ position: 'absolute' }}>Loading Data</h3>
-			<svg style={styles.svg} focusable='false' viewBox='0 0 40 40'>
+		<div
+			style={{
+				display: 'flex',
+				position: 'relative',
+				justifyContent: 'center',
+				alignItems: 'center',
+				flex: 1,
+			}}
+		>
+			{text && (
+				<h3
+					style={{
+						position: 'absolute',
+						maxWidth: '50%',
+						textAlign: 'center',
+						boxSizing: 'border-box',
+					}}
+				>
+					{text}
+				</h3>
+			)}
+			<svg
+				style={{ width: '256px', height: '256px' }}
+				focusable='false'
+				viewBox='0 0 40 40'
+			>
 				<path
 					fill='#4267b2'
 					d='M20.201,5.169c-8.254,0-14.946,6.692-14.946,14.946c0,8.255,6.692,14.946,14.946,14.946
