5ce60836379af8cf5697241a2617b81b5c239362
mrjackmaster <jacektrg+github@gmail.com>
2022-03-24 19:55:42 +0100

    Adding option for choosing long or short format of returned string, defaults to long format.

--- src/components/utils/timeSinceDate.tsx
+++ src/components/utils/timeSinceDate.tsx
@@ -2,45 +2,71 @@
 
 import { DateTime } from 'luxon';
 
-const timeSinceDate = (timestamp: string) => {
+const timeSinceDate = (date: string, shortFormat: boolean = false) => {
+	if (!date) {
+		return;
+	}
+	if (!shortFormat) {
+		shortFormat = false;
+	}
 	const currentDate = DateTime.now();
-	const pastDate = DateTime.fromISO(timestamp, 'dd LLLL yyyy');
+	const pastDate = DateTime.fromISO(date);
 	const { years, months, days, hours, minutes } = currentDate
 		.diff(pastDate, ['years', 'months', 'days', 'hours', 'minutes'])
 		.toObject();
-	if (years > 1) {
-		if (years === 1) {
-			return `${years} year ago`;
+	let string = '';
+	if (!shortFormat) {
+		if (years > 1) {
+			if (years === 1) {
+				string = `${years} year ago`;
+			} else {
+				string = `${years} years ago`;
+			}
+		} else if (months > 1) {
+			if (months === 1) {
+				string = `${months} month ago`;
+			} else {
+				string = `${months} months ago`;
+			}
+		} else if (days > 1) {
+			if (days === 1) {
+				string = `${days} day ago`;
+			} else {
+				string = `${days} days ago`;
+			}
+		} else if (hours > 1) {
+			if (hours === 1) {
+				string = `${hours} hour ago`;
+			} else {
+				string = `${hours} hours ago`;
+			}
 		} else {
-			return `${years} years ago`;
-		}
-	} else if (months > 1) {
-		if (months === 1) {
-			return `${months} month ago`;
-		} else {
-			return `${months} months ago`;
-		}
-	} else if (days > 1) {
-		if (days === 1) {
-			return `${days} day ago`;
-		} else {
-			return `${days} days ago`;
-		}
-	} else if (hours > 1) {
-		if (hours === 1) {
-			return `${hours} hour ago`;
-		} else {
-			return `${hours} hours ago`;
+			if (minutes <= 1) {
+				string = `just now`;
+			} else if (minutes === 1) {
+				string = `${Math.round(minutes)} minute ago`;
+			} else {
+				string = `${Math.round(minutes)} minutes ago`;
+			}
 		}
 	} else {
-		if (minutes <= 1) {
-			return `just now`;
-		} else if (minutes === 1) {
-			return `${Math.round(minutes)} minute ago`;
+		if (years > 1) {
+			string = `${years} y`;
+		} else if (months > 1) {
+			string = `${months} m`;
+		} else if (days > 1) {
+			string = `${days} d`;
+		} else if (hours > 1) {
+			string = `${hours} h`;
 		} else {
-			return `${Math.round(minutes)} minutes ago`;
+			if (minutes <= 1) {
+				string = `now`;
+			} else {
+				string = `${Math.round(minutes)} min`;
+			}
 		}
 	}
+	return string;
 };
 
 export default timeSinceDate;
