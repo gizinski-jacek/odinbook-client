eb9c8d6a725eb1c138a7682e00daa3bf9dadd803
mrjackmaster <jacektrg+github@gmail.com>
2022-03-29 18:21:09 +0200

    No functionality changes

--- src/components/utils/axiosFunctions.tsx
+++ src/components/utils/axiosFunctions.tsx
@@ -3,25 +3,33 @@ import axios from 'axios';
 const options: {} = { withCredentials: true };
 
 const axiosGet = async (url: string, opt = options) => {
-	if (!url) return;
+	if (!url) {
+		return;
+	}
 	const res = await axios.get(url, opt);
 	return res.data;
 };
 
 const axiosPost = async (url: string, data: {}, opt = options) => {
-	if (!url || !data) return;
+	if (!url || !data) {
+		return;
+	}
 	const res = await axios.post(url, data, opt);
 	return res.data;
 };
 
 const axiosPut = async (url: string, data: {}, opt = options) => {
-	if (!url || !data) return;
+	if (!url || !data) {
+		return;
+	}
 	const res = await axios.put(url, data, opt);
 	return res.data;
 };
 
 const axiosDelete = async (url: string, opt = options) => {
-	if (!url) return;
+	if (!url) {
+		return;
+	}
 	const res = await axios.delete(url, opt);
 	return res.data;
 };
