cbd962cbb5c36c2b03f676aa2110f3c1ce8dea15
mrjackmaster <jacektrg+github@gmail.com>
2022-03-30 22:06:31 +0200

    Renaming window listeners to better reflect what they're listening for. Updating onClick events. Removing not-implemented reply button and reply function.

--- src/components/utils/CommentWrapper.tsx
+++ src/components/utils/CommentWrapper.tsx
@@ -25,13 +25,13 @@ const CommentWrapper: React.FC<Props> = ({ comment }) => {
 	const toggleOptions = (e: React.MouseEvent<HTMLSpanElement>) => {
 		e.stopPropagation();
 		setShowOptions((prevState) => !prevState);
-		document.addEventListener('click', windowListener);
+		document.addEventListener('click', windowOptionsListener);
 	};
 
-	const windowListener = (e: any) => {
+	const windowOptionsListener = (e: any) => {
 		e.stopPropagation();
 		if (optionsRef.current !== e.target) {
-			document.removeEventListener('click', windowListener);
+			document.removeEventListener('click', windowOptionsListener);
 			setShowOptions(false);
 		}
 	};
@@ -58,7 +58,12 @@ const CommentWrapper: React.FC<Props> = ({ comment }) => {
 		setShowModal(false);
 	};
 
-	const handleLike = async (commentPostRef: string, commentId: string) => {
+	const handleLike = async (
+		e: React.MouseEvent<HTMLDivElement>,
+		commentPostRef: string,
+		commentId: string
+	) => {
+		e.stopPropagation();
 		try {
 			setCommentData(
 				await axiosPut(
@@ -66,14 +71,6 @@ const CommentWrapper: React.FC<Props> = ({ comment }) => {
 					{ commentPostRef, commentId }
 				)
 			);
-		} catch (error: any) {
-			console.error(error);
-		}
-	};
-
-	const handleReply = async (commentId: string) => {
-		try {
-			//
 		} catch (error: any) {
 			console.error(error);
 		}
@@ -127,8 +124,8 @@ const CommentWrapper: React.FC<Props> = ({ comment }) => {
 							{commentData.likes.includes(user._id) && (
 								<div
 									className={styles.liked}
-									onClick={() =>
-										handleLike(commentData.post_ref, commentData._id)
+									onClick={(e) =>
+										handleLike(e, commentData.post_ref, commentData._id)
 									}
 								>
 									<span></span>
@@ -140,7 +137,7 @@ const CommentWrapper: React.FC<Props> = ({ comment }) => {
 								<>
 									<span
 										className={styles.options_toggle}
-										onClick={toggleOptions}
+										onClick={(e) => toggleOptions(e)}
 									>
 										<svg viewBox='0 0 20 20' width='20' height='20'>
 											<g transform='translate(-446 -350)'>
@@ -150,12 +147,15 @@ const CommentWrapper: React.FC<Props> = ({ comment }) => {
 									</span>
 									{showOptions && (
 										<div ref={optionsRef} className={styles.options_menu}>
-											<div className={styles.edit_btn} onClick={openEditForm}>
+											<div
+												className={styles.edit_btn}
+												onClick={(e) => openEditForm(e)}
+											>
 												Edit comment
 											</div>
 											<div
 												className={styles.delete_btn}
-												onClick={openDeleteModal}
+												onClick={(e) => openDeleteModal(e)}
 											>
 												Delete comment
 											</div>
@@ -170,21 +170,18 @@ const CommentWrapper: React.FC<Props> = ({ comment }) => {
 							className={`${styles.like_btn} ${
 								commentData.likes.includes(user._id) ? styles.liked : ''
 							}`}
-							onClick={() => handleLike(commentData.post_ref, commentData._id)}
+							onClick={(e) =>
+								handleLike(e, commentData.post_ref, commentData._id)
+							}
 						>
 							Like
-						</div>
-						<div
-							className={styles.reply_btn}
-							onClick={() => handleReply(commentData._id)}
-						>
-							Reply
 						</div>
 						<div className={styles.time}>
 							{timeSinceDate(commentData.createdAt)}
 						</div>
 					</span>
 				</div>
+
 				{showModal && (
 					<DeleteModal
 						closeModal={closeDeleteModal}
