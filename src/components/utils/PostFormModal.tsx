9276a833029647a3ffffd05ba3ce2b98e706d410
mrjackmaster <jacektrg+github@gmail.com>
2022-04-01 23:20:41 +0200

    Adding functionality handling selecting picture file and submitting it. Rendering chosen picture or existing post picture. Updating props and props types.

--- src/components/utils/PostFormModal.tsx
+++ src/components/utils/PostFormModal.tsx
@@ -1,29 +1,45 @@
-import { useContext, useState } from 'react';
+import { useContext, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { UserContext } from '../hooks/UserProvider';
-import { PostFull, PostNew } from '../../myTypes';
+import { FormError, PostFull, PostNew } from '../../myTypes';
 import { axiosPost, axiosPut } from './axiosFunctions';
+import FormErrorWrapper from './FormErrorWrapper';
 import styles from '../../styles/PostFormModal.module.scss';
 
 type Props = {
 	closeModal: (
-		e: React.MouseEvent<HTMLElement> | React.FormEvent<HTMLFormElement>,
-		data: PostFull | PostNew
+		e:
+			| React.MouseEvent<HTMLSpanElement | HTMLButtonElement>
+			| React.FormEvent<HTMLFormElement>,
+		data: PostNew | PostFull,
+		pictureData?: any
 	) => void;
-	setTimeline?: (data: PostFull[]) => void;
-	setPost?: (data: PostFull) => void;
-	post: PostFull | PostNew;
+	updateTimeline?: (
+		e: React.FormEvent<HTMLFormElement>,
+		data: PostFull[]
+	) => void;
+	updatePost?: (e: React.FormEvent<HTMLFormElement>, data: PostFull) => void;
+	postData: PostNew | PostFull;
+	postPictureData?: any;
 };
 
 const PostFormModal: React.FC<Props> = ({
 	closeModal,
-	setTimeline,
-	setPost,
-	post,
+	updateTimeline,
+	updatePost,
+	postData,
+	postPictureData,
 }) => {
 	const { user } = useContext(UserContext);
 
-	const [formData, setFormData] = useState<PostFull | PostNew>(post);
+	const pictureRef = useRef<HTMLInputElement>(null);
+
+	const [errors, setErrors] = useState<FormError[]>([]);
+	const [formData, setFormData] = useState<PostFull | PostNew>(postData);
+	const [pictureData, setPictureData] = useState<{
+		preview: string;
+		data: {};
+	} | null>(postPictureData || null);
 
 	const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
 		const { name, value } = e.target;
@@ -33,39 +49,94 @@ const PostFormModal: React.FC<Props> = ({
 		}));
 	};
 
+	const clickSelectFile = (
+		e: React.MouseEvent<HTMLButtonElement | HTMLDivElement>
+	) => {
+		e.stopPropagation();
+		pictureRef.current?.click();
+	};
+
+	const handleFileChange = (e: any) => {
+		e.stopPropagation();
+		setErrors([]);
+		const file = e.target.files[0];
+		if (file.size > 2000000) {
+			setErrors([{ msg: 'File too large' }]);
+			return;
+		}
+		if (
+			file.type !== 'image/png' &&
+			file.type !== 'image/jpg' &&
+			file.type !== 'image/jpeg'
+		) {
+			setErrors([{ msg: 'Only images (png, jpg, jpeg) are allowed' }]);
+			return;
+		}
+		setPictureData({
+			preview: URL.createObjectURL(file),
+			data: file,
+		});
+	};
+
+	const handleRemoveFile = (e: React.MouseEvent<HTMLButtonElement>) => {
+		e.stopPropagation();
+		setPictureData(null);
+	};
+
 	const handleSubmit = async (
 		e: React.FormEvent<HTMLFormElement>,
-		data: PostFull | PostNew
+		data: PostFull | PostNew,
+		pictureData?: any
 	) => {
 		e.preventDefault();
 		try {
-			if (post._id && setPost) {
-				setPost(await axiosPut(`/api/posts/${data._id}`, data));
-			} else if (setTimeline) {
-				setTimeline(await axiosPost('/api/posts', data));
+			const postData: any = new FormData();
+			for (const [key, value] of Object.entries(data)) {
+				postData.append(key, value);
+			}
+			postData.append('post_picture', pictureData);
+			if (data._id && updatePost) {
+				updatePost(e, await axiosPut(`/api/posts/${data._id}`, postData));
+			} else if (updateTimeline) {
+				updateTimeline(e, await axiosPost('/api/posts', postData));
 			}
 			closeModal(e, { text: '' });
 		} catch (error: any) {
-			console.error(error);
+			if (!Array.isArray(error.response.data)) {
+				if (typeof error.response.data === 'object') {
+					setErrors([error.response.data]);
+					return;
+				}
+				if (typeof error.response.data === 'string') {
+					setErrors([{ msg: error.response.data }]);
+					return;
+				}
+			} else {
+				setErrors(error.response.data);
+			}
 		}
 	};
+
+	const errorsDisplay = errors.map((error, index) => {
+		return <FormErrorWrapper key={index} error={error} />;
+	});
 
 	return (
 		user && (
 			<div className={styles.modal_container}>
 				<span
 					className={styles.grayout_bg}
-					onClick={(e) => closeModal(e, formData)}
+					onClick={(e) => closeModal(e, formData, pictureData)}
 				></span>
 				<div className={styles.new_post_container}>
 					<div className={styles.top}>
 						<div className={styles.title}>
-							<h3>{post._id ? 'Update post' : 'Create post'}</h3>
+							<h3>{postData._id ? 'Update post' : 'Create post'}</h3>
 						</div>
 						<button
 							type='button'
 							className={styles.close_btn}
-							onClick={(e) => closeModal(e, formData)}
+							onClick={(e) => closeModal(e, formData, pictureData)}
 						>
 							<span></span>
 						</button>
@@ -80,25 +151,67 @@ const PostFormModal: React.FC<Props> = ({
 							{user.first_name} {user.last_name}
 						</h4>
 					</span>
-					<div className={styles.post_form}>
-						<form onSubmit={(e) => handleSubmit(e, formData)}>
+					<div className={styles.body}>
+						<form
+							encType='multipart/form-data'
+							onSubmit={(e) => handleSubmit(e, formData, pictureData?.data)}
+						>
 							<textarea
 								name='text'
 								minLength={1}
 								maxLength={512}
-								rows={12}
+								rows={10}
 								onChange={handleChange}
 								value={formData.text}
 								required
 								autoFocus
 								placeholder={`What's on your mind, ${user.first_name}?`}
 							/>
+							{pictureData && (
+								<div
+									className={styles.post_picture}
+									onClick={(e) => clickSelectFile(e)}
+								>
+									<img
+										src={
+											pictureData.preview ||
+											`http://localhost:4000/photos/posts/${formData.picture}`
+										}
+										alt='Post pic'
+									/>
+								</div>
+							)}
+							<input
+								style={{ display: 'none' }}
+								ref={pictureRef}
+								type='file'
+								name='post_picture'
+								onChange={handleFileChange}
+							/>
+							{errorsDisplay && <ul className='error-list'>{errorsDisplay}</ul>}
+							{pictureData ? (
+								<button
+									type='button'
+									className='btn-default btn-danger'
+									onClick={(e) => handleRemoveFile(e)}
+								>
+									Remove Picture
+								</button>
+							) : (
+								<button
+									type='button'
+									className='btn-default btn-active'
+									onClick={(e) => clickSelectFile(e)}
+								>
+									Add Picture
+								</button>
+							)}
 							<button
 								type='submit'
 								className='btn-default btn-form-submit'
 								disabled={formData.text ? false : true}
 							>
-								{post._id ? 'Save' : 'Post'}
+								{postData._id ? 'Save' : 'Post'}
 							</button>
 						</form>
 					</div>
