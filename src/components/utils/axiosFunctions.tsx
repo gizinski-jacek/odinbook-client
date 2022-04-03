2f4f86f44248d4dcc1c71f2dc4ec919847f44eb2
mrjackmaster <jacektrg+github@gmail.com>
2022-04-04 01:12:37 +0200

    Minor update. Spreading options passed as parameter and built in default options

--- src/components/utils/axiosFunctions.tsx
+++ src/components/utils/axiosFunctions.tsx
@@ -1,36 +1,36 @@
 import axios from 'axios';
 
-const options: {} = { withCredentials: true };
+const options = { withCredentials: true };
 
-const axiosGet = async (url: string, opt = options) => {
+const axiosGet = async (url: string, opt?: {}) => {
 	if (!url) {
 		return;
 	}
-	const res = await axios.get(url, opt);
+	const res = await axios.get(url, { ...options, ...opt });
 	return res.data;
 };
 
-const axiosPost = async (url: string, data: {}, opt = options) => {
+const axiosPost = async (url: string, data: {}, opt?: {}) => {
 	if (!url || !data) {
 		return;
 	}
-	const res = await axios.post(url, data, opt);
+	const res = await axios.post(url, data, { ...options, ...opt });
 	return res.data;
 };
 
-const axiosPut = async (url: string, data: {}, opt = options) => {
+const axiosPut = async (url: string, data: {}, opt?: {}) => {
 	if (!url || !data) {
 		return;
 	}
-	const res = await axios.put(url, data, opt);
+	const res = await axios.put(url, data, { ...options, ...opt });
 	return res.data;
 };
 
-const axiosDelete = async (url: string, opt = options) => {
+const axiosDelete = async (url: string, opt?: {}) => {
 	if (!url) {
 		return;
 	}
-	const res = await axios.delete(url, opt);
+	const res = await axios.delete(url, { ...options, ...opt });
 	return res.data;
 };
 
